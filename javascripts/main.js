5ce5320451c0cd83ad6370543d2b1473813de2fb
Richard Park <rpark@linkedin.com>
2013-04-26 18:00:28 -0700

    Making more robust landing page and license page.

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,13 @@
-console.log('This would be the main JS file.');
+$(function() {
+	// For page selection highlighter
+	var page = $("#page");
+	if (page.length) {
+		var pageId = page.attr("pageid");
+		if (pageId) {
+			var nav = $("#nav-" + pageId);
+			if (nav.length) {
+				nav.addClass("selected");
+			}
+		}
+	}
+});
