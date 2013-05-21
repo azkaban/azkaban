43a1d124fbccc03b36c446b35b4233c6a0fe46dd
Lishid <lishid@gmail.com>
2013-05-21 14:27:11 -0700

    Added history viewing for previous executions on the schedule graph.

--- src/java/azkaban/utils/Utils.java
+++ src/java/azkaban/utils/Utils.java
@@ -277,4 +277,12 @@ public class Utils {
 		Method method = clazz.getDeclaredMethod(methodName, argTypes);
 		return method.invoke(null, args);
 	}
+	
+	public static void copyStream(InputStream input, OutputStream output) throws IOException {
+		byte[] buffer = new byte[1024];
+		int bytesRead;
+		while ((bytesRead = input.read(buffer)) != -1) {
+			output.write(buffer, 0, bytesRead);
+		}
+	}
 }
