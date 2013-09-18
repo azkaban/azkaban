68d1b18cbdf29d41f945185d2d3f40d239821f55
David Z. Chen <dchen@linkedin.com>
2013-09-17 18:11:45 -0700

    Add comment headers to all source files.

--- src/java/azkaban/jobExecutor/utils/process/ProcessFailureException.java
+++ src/java/azkaban/jobExecutor/utils/process/ProcessFailureException.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012 LinkedIn, Inc
+ * Copyright 2012 LinkedIn Corp.
  * 
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
@@ -13,6 +13,7 @@
  * License for the specific language governing permissions and limitations under
  * the License.
  */
+
 package azkaban.jobExecutor.utils.process;
 
 public class ProcessFailureException extends RuntimeException {
