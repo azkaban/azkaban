39c8500e7903db6466884a3c5e761fc1a26fb077
David Z. Chen <david.z.chen@outlook.com>
2013-11-16 09:00:13 -0800

    Add Bootstrap resources. Redesign index and login webapp pages.

--- src/web/js/azkaban.login.js
+++ src/web/js/azkaban.login.js
@@ -17,45 +17,53 @@
 $.namespace('azkaban');
 
 var loginView;
-azkaban.LoginView= Backbone.View.extend({
-  events : {
-    "click #loginSubmit": "handleLogin",
-    'keypress input': 'handleKeyPress'
-  },
-  initialize : function(settings) {
-	 $('#errorMsg').hide();
-  },
-  handleLogin : function(evt) {
-	 console.log("Logging in.");
-	 var username = $("#username").val();
-	 var password = $("#password").val();
+azkaban.LoginView = Backbone.View.extend({
+	events: {
+		"click #login-submit": "handleLogin",
+		'keypress input': 'handleKeyPress'
+	},
+
+	initialize: function(settings) {
+		$('#error-msg').hide();
+	},
+	
+	handleLogin: function(evt) {
+		console.log("Logging in.");
+		var username = $("#username").val();
+		var password = $("#password").val();
 	 
-     $.ajax({
-     	async: "false",
-     	url: contextURL,
-     	dataType: "json",
-     	type: "POST",
-     	data: {action:"login", username:username, password:password},
-     	success: function(data) {
-     		if (data.error) {
-     			$('#errorMsg').text(data.error);
-     			$('#errorMsg').slideDown('fast');
-     		}
-     		else {
-     			document.location.reload();
-     		}
-     	}
-     });
-  },
-  handleKeyPress : function(evt) {
-	  if (evt.charCode == 13 || evt.keyCode == 13) {
-	  	this.handleLogin();
-  	  }
-  },
-  render: function() {
-  }
+		$.ajax({
+			async: "false",
+			url: contextURL,
+			dataType: "json",
+			type: "POST",
+			data: {
+				action: "login", 
+				username: username, 
+				password: password
+			},
+			success: function(data) {
+				if (data.error) {
+					$('#error-msg').text(data.error);
+					$('#error-msg').slideDown('fast');
+				}
+				else {
+					document.location.reload();
+				}
+			}
+		});
+	},
+	
+	handleKeyPress: function(evt) {
+		if (evt.charCode == 13 || evt.keyCode == 13) {
+			this.handleLogin();
+		}
+	},
+	
+	render: function() {
+	}
 });
 
 $(function() {
-	loginView = new azkaban.LoginView({el:$('#loginForm')});
+	loginView = new azkaban.LoginView({el: $('#login-form')});
 });
