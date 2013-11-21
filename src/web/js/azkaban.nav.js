2f257259b9404771943a8ed7da48fa7fdf8d3529
David Z. Chen <david.z.chen@outlook.com>
2013-11-20 17:20:53 -0800

    Style changes.

--- src/web/js/azkaban.nav.js
+++ src/web/js/azkaban.nav.js
@@ -18,13 +18,15 @@ $.namespace('azkaban');
 
 var navView;
 azkaban.NavView = Backbone.View.extend({
-	events : {
+	events: {
 		"click #user-id":"handleUserMenu"
 	},
-	initialize : function(settings) {
+	
+	initialize: function(settings) {
 		$("#user-menu").hide();
 	},
-	handleUserMenu : function(evt) {
+	
+	handleUserMenu: function(evt) {
 		if ($("#user-menu").is(":visible")) {
 			$("#user-menu").slideUp('fast');
 		}
@@ -32,21 +34,24 @@ azkaban.NavView = Backbone.View.extend({
 			$("#user-menu").slideDown('fast');
 		}
 	},
+	
 	render: function() {
 	}
 });
 
 $(function() {
-	navView = new azkaban.NavView({el:$( '#header' )});
+	navView = new azkaban.NavView({
+		el: $('#header')
+	});
 });
 
 (function($){
-	$.getQuery = function( query ) {
+	$.getQuery = function(query) {
 		query = query.replace(/[\[]/,"\\\[").replace(/[\]]/,"\\\]");
 		var expr = "[\\?&]"+query+"=([^&#]*)";
-		var regex = new RegExp( expr );
-		var results = regex.exec( window.location.href );
-		if( results !== null ) {
+		var regex = new RegExp(expr);
+		var results = regex.exec(window.location.href);
+		if (results !== null) {
 			return results[1];
 			return decodeURIComponent(results[1].replace(/\+/g, " "));
 		} else {
