5040c45804df2795cf47654a46a8304437492fa5
David Z. Chen <david.z.chen@outlook.com>
2014-03-05 16:02:23 -0800

    Issue #204 - Make time values on JMX page consistent.

--- src/web/js/azkaban/util/date.js
+++ src/web/js/azkaban/util/date.js
@@ -1,12 +1,12 @@
 /*
  * Copyright 2012 LinkedIn Corp.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
  * the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
@@ -14,12 +14,14 @@
  * the License.
  */
 
+var TIMESTAMP_LENGTH = 13;
+
 var getDuration = function(startMs, endMs) {
 	if (startMs) {
 		if (endMs == null || endMs < startMs) {
 			return "-";
 		}
-		
+
 		var diff = endMs - startMs;
 		return formatDuration(diff, false);
 	}
@@ -30,7 +32,7 @@ var getDuration = function(startMs, endMs) {
 var formatDuration = function(duration, millisecSig) {
 	var diff = duration;
 	var seconds = Math.floor(diff / 1000);
-	
+
 	if (seconds < 60) {
 		if (millisecSig) {
 			return (diff / 1000).toFixed(millisecSig) + " s";
@@ -39,7 +41,7 @@ var formatDuration = function(duration, millisecSig) {
 			return seconds + " sec";
 		}
 	}
-	
+
 	var mins = Math.floor(seconds / 60);
 	seconds = seconds % 60;
 	if (mins < 60) {
@@ -51,10 +53,10 @@ var formatDuration = function(duration, millisecSig) {
 	if (hours < 24) {
 		return hours + "h " + mins + "m " + seconds + "s";
 	}
-	
+
 	var days = Math.floor(hours / 24);
 	hours = hours % 24;
-	
+
 	return days + "d " + hours + "h " + mins + "m";
 }
 
@@ -62,12 +64,12 @@ var getDateFormat = function(date) {
 	var year = date.getFullYear();
 	var month = getTwoDigitStr(date.getMonth() + 1);
 	var day = getTwoDigitStr(date.getDate());
-	
+
 	var hours = getTwoDigitStr(date.getHours());
 	var minutes = getTwoDigitStr(date.getMinutes());
 	var second = getTwoDigitStr(date.getSeconds());
 
-	var datestring = year + "-" + month + "-" + day + "  " + hours + ":" + 
+	var datestring = year + "-" + month + "-" + day + "  " + hours + ":" +
 			minutes + " " + second + "s";
 	return datestring;
 }
@@ -76,7 +78,7 @@ var getHourMinSec = function(date) {
 	var hours = getTwoDigitStr(date.getHours());
 	var minutes = getTwoDigitStr(date.getMinutes());
 	var second = getTwoDigitStr(date.getSeconds());
-	
+
 	var timestring = hours + ":" + minutes + " " + second + "s";
 	return timestring;
 }
@@ -85,6 +87,6 @@ var getTwoDigitStr = function(value) {
 	if (value < 10) {
 		return "0" + value;
 	}
-	
+
 	return value;
 }
