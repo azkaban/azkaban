c53c0910fad35f925f2c060dc5201e2746464d89
Gaurav Aggarwal <gaggarwal@linkedin.com>
2015-09-14 17:51:39 -0700

    Removing ActiveExecutorsManager

--- azkaban-common/src/main/java/azkaban/executor/ActiveExecutorsManager.java
+++
@@ -1,9 +0,0 @@
-package azkaban.executor;
-
-public class ActiveExecutorsManager {
-
-  public ActiveExecutorsManager() {
-    // TODO Auto-generated constructor stub
-  }
-
-}
--- azkaban-common/src/main/java/azkaban/executor/ExecutorManager.java
+++ azkaban-common/src/main/java/azkaban/executor/ExecutorManager.java
@@ -32,19 +32,16 @@ import java.util.Set;
 import java.util.TreeMap;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
+
 import org.apache.commons.lang.StringUtils;
 import org.apache.log4j.Logger;
 import org.joda.time.DateTime;
+
 import azkaban.alert.Alerter;
 import azkaban.event.Event;
 import azkaban.event.Event.Type;
