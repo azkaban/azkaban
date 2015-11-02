c993f4e20985e84179b24d5de0c711075cc9a634
Vikram Kone <vikone@microsoft.com>
2015-11-02 07:55:25 -0800

    Replace all tokens for call back params
    Currently JobCall back params are replaced only for the first instance.
    This changes ensures that all the tokens in the body and headers and
    replaced..

--- azkaban-execserver/src/main/java/azkaban/execapp/event/JobCallbackUtil.java
+++ azkaban-execserver/src/main/java/azkaban/execapp/event/JobCallbackUtil.java
@@ -181,6 +181,13 @@ public class JobCallbackUtil {
         String headersKeyPerSequence =
             headersKey.replace(SEQUENCE_TOKEN, sequenceStr);
         String headersValue = props.get(headersKeyPerSequence);
+		
+		// replace all the tokens in the header
+		if(headersValue != null)
+		{
+			headersValue = replaceTokens(headersValue, contextInfo, false);
+		}
+		
         privateLogger.info("headers: " + headersValue);
         Header[] headers = parseHttpHeaders(headersValue);
         if (headers != null) {
@@ -287,25 +294,25 @@ public class JobCallbackUtil {
     String result = value;
     String tokenValue =
         encodeQueryParam(contextInfo.get(CONTEXT_SERVER_TOKEN), withEncoding);
-    result = result.replaceFirst(Pattern.quote(CONTEXT_SERVER_TOKEN), tokenValue);
+    result = result.replaceAll(Pattern.quote(CONTEXT_SERVER_TOKEN), tokenValue);
 
     tokenValue = encodeQueryParam(contextInfo.get(CONTEXT_PROJECT_TOKEN), withEncoding);
-    result = result.replaceFirst(Pattern.quote(CONTEXT_PROJECT_TOKEN), tokenValue);
+    result = result.replaceAll(Pattern.quote(CONTEXT_PROJECT_TOKEN), tokenValue);
 
     tokenValue = encodeQueryParam(contextInfo.get(CONTEXT_FLOW_TOKEN), withEncoding);
-    result = result.replaceFirst(Pattern.quote(CONTEXT_FLOW_TOKEN), tokenValue);
+    result = result.replaceAll(Pattern.quote(CONTEXT_FLOW_TOKEN), tokenValue);
 
     tokenValue = encodeQueryParam(contextInfo.get(CONTEXT_JOB_TOKEN), withEncoding);
-    result = result.replaceFirst(Pattern.quote(CONTEXT_JOB_TOKEN), tokenValue);
+    result = result.replaceAll(Pattern.quote(CONTEXT_JOB_TOKEN), tokenValue);
 
     tokenValue =
         encodeQueryParam(contextInfo.get(CONTEXT_EXECUTION_ID_TOKEN), withEncoding);
-    result = result.replaceFirst(Pattern.quote(CONTEXT_EXECUTION_ID_TOKEN), tokenValue);
+    result = result.replaceAll(Pattern.quote(CONTEXT_EXECUTION_ID_TOKEN), tokenValue);
 
     tokenValue =
         encodeQueryParam(contextInfo.get(CONTEXT_JOB_STATUS_TOKEN), withEncoding);
 
-    result = result.replaceFirst(Pattern.quote(CONTEXT_JOB_STATUS_TOKEN), tokenValue);
+    result = result.replaceAll(Pattern.quote(CONTEXT_JOB_STATUS_TOKEN), tokenValue);
 
     return result;
   }
