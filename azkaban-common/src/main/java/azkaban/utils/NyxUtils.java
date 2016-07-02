aff63b68eb698cc42a44859ab22bec50e799d741
Evan Li <evli@evli-mn1.linkedin.biz>
2016-07-01 22:49:25 -0700

    Bug fix - not to register trigger when the schedule is set, add feature to disable Trigger status button when the nyxTrigger doesnot exist in the project pack, some ui updates etc.

--- azkaban-common/src/main/java/azkaban/utils/NyxUtils.java
+++ azkaban-common/src/main/java/azkaban/utils/NyxUtils.java
@@ -29,7 +29,6 @@ import azkaban.executor.ExecutorApiClient;
 import azkaban.trigger.TriggerManager;
 import azkaban.trigger.TriggerManagerException;
 
-
 /**
  * Helper class wrapping REST API client for Nyx Service
  *
@@ -37,129 +36,152 @@ import azkaban.trigger.TriggerManagerException;
  *
  */
 public class NyxUtils {
-  private static Logger logger = Logger.getLogger(NyxUtils.class);
-  public static final String NYX_SERVER_PORT = "nyx.service.port";
-  public static final String NYX_SERVER_HOST = "nyx.service.host";
+	private static Logger logger = Logger.getLogger(NyxUtils.class);
+	public static final String NYX_SERVER_PORT = "nyx.service.port";
+	public static final String NYX_SERVER_HOST = "nyx.service.host";
 
-  private static String nyxServiceHost = "localhost";
-  private static final boolean isHttp = true;
-  private static int port = 8080;
+	private static String nyxServiceHost = "localhost";
+	private static final boolean isHttp = true;
+	private static int port = 8080;
 
-  static {
-    // populating nyx service from .properties configs
-    Props props = TriggerManager.getAzprops();
-    if (props != null) {
-      nyxServiceHost = props.getString(NYX_SERVER_HOST, nyxServiceHost);
-      port = props.getInt(NYX_SERVER_PORT, port);
-    }
-  }
+	static {
+		// populating nyx service from .properties configs
+		Props props = TriggerManager.getAzprops();
+		if (props != null) {
+			nyxServiceHost = props.getString(NYX_SERVER_HOST, nyxServiceHost);
+			port = props.getInt(NYX_SERVER_PORT, port);
+		}
+	}
 
-  /**
-   * Use trigger json specification to register a trigger with Nyx Service
-   *
-   * @throws TriggerManagerException
-   */
-  public static long registerNyxTrigger(String specificationJson) throws TriggerManagerException {
-    try {
-      ExecutorApiClient client = ExecutorApiClient.getInstance();
-      URI uri = ExecutorApiClient.buildUri(nyxServiceHost, port, "/register", isHttp);
-      List<NameValuePair> headers = new ArrayList<>();
-      headers.add(new BasicNameValuePair("Content-Type", "application/json"));
-      String rawResponse = client.httpPost(uri, headers, specificationJson);
-      Map<String, Object> parsedResponse = (Map<String, Object>) JSON.parse(rawResponse);
+	/**
+	 * Use trigger json specification to register a trigger with Nyx Service
+	 *
+	 * @throws TriggerManagerException
+	 */
+	public static long registerNyxTrigger(String specificationJson)
+			throws TriggerManagerException {
+		try {
+			ExecutorApiClient client = ExecutorApiClient.getInstance();
+			URI uri = ExecutorApiClient.buildUri(nyxServiceHost, port,
+					"/register", isHttp);
+			List<NameValuePair> headers = new ArrayList<>();
+			headers.add(new BasicNameValuePair("Content-Type",
+					"application/json"));
+			String rawResponse = client.httpPost(uri, headers,
+					specificationJson);
+			Map<String, Object> parsedResponse = (Map<String, Object>) JSON
+					.parse(rawResponse);
 
-      // TODO: to be revisited. Presence of an "id" field signify successfully
-      // registration of trigger
-      if (parsedResponse.containsKey("error")) {
-        throw new IllegalArgumentException((String) parsedResponse.get("error"));
-      } else if (parsedResponse.containsKey("id")) {
-        return (Long) parsedResponse.get("id");
-      } else {
-        throw new TriggerManagerException("Failed to parse Nyx response " + rawResponse);
-      }
-    } catch (Exception ex) {
-      logger.error("Failed to get Nyx service response for :" + specificationJson, ex);
-      throw new TriggerManagerException(ex);
-    }
-  }
+			// TODO: to be revisited. Presence of an "id" field signify
+			// successfully
+			// registration of trigger
+			if (parsedResponse.containsKey("error")) {
+				throw new IllegalArgumentException(
+						(String) parsedResponse.get("error"));
+			} else if (parsedResponse.containsKey("id")) {
+				return (Long) parsedResponse.get("id");
+			} else {
+				throw new TriggerManagerException(
+						"Failed to parse Nyx response " + rawResponse);
+			}
+		} catch (Exception ex) {
+			logger.error("Failed to get Nyx service response for :"
+					+ specificationJson, ex);
+			throw new TriggerManagerException(ex);
+		}
+	}
 
-  /**
-   * Delete an already registered trigger from Nyx Service
-   *
-   * @throws TriggerManagerException
-   */
-  public static void unregisterNyxTrigger(Long triggerId) throws TriggerManagerException {
-    if (triggerId == -1) {
-      throw new TriggerManagerException("Trigger is not registered");
-    }
+	/**
+	 * Delete an already registered trigger from Nyx Service
+	 *
+	 * @throws TriggerManagerException
+	 */
+	public static void unregisterNyxTrigger(Long triggerId)
+			throws TriggerManagerException {
+		if (triggerId == -1) {
+			logger.info("skipping unregistering nyx trigger as the trigger is not yet registered with service.");
+			return;
+		}
 
-    try {
-      ExecutorApiClient client = ExecutorApiClient.getInstance();
-      URI uri = ExecutorApiClient.buildUri(nyxServiceHost, port, "/unregister/" + triggerId, isHttp);
-      String response = client.httpGet(uri, null);
-      Map<String, Object> parsedResponse = (Map<String, Object>) JSON.parse(response);
+		try {
+			ExecutorApiClient client = ExecutorApiClient.getInstance();
+			URI uri = ExecutorApiClient.buildUri(nyxServiceHost, port,
+					"/unregister/" + triggerId, isHttp);
+			String response = client.httpGet(uri, null);
+			Map<String, Object> parsedResponse = (Map<String, Object>) JSON
+					.parse(response);
 
-      if (parsedResponse.containsKey("error")) {
-        throw new Exception((String) parsedResponse.get("error"));
-      }
-    } catch (Exception ex) {
-      logger.error("Failed to get Nyx service response for triggerId : " + triggerId, ex);
-      throw new TriggerManagerException(ex);
-    }
-  }
+			if (parsedResponse.containsKey("error")) {
+				throw new Exception((String) parsedResponse.get("error"));
+			}
+		} catch (Exception ex) {
+			logger.error("Failed to get Nyx service response for triggerId : "
+					+ triggerId, ex);
+			throw new TriggerManagerException(ex);
+		}
+	}
 
-  /**
-   * Look up status of an already registered trigger
-   *
-   * @param triggerId
-   * @return status fetched from Nyx
-   * @throws TriggerManagerException
-   */
-  public static boolean isNyxTriggerReady(Long triggerId) throws TriggerManagerException {
-    if (triggerId == -1) {
-      throw new TriggerManagerException("Trigger is not registered");
-    }
+	/**
+	 * Look up status of an already registered trigger
+	 *
+	 * @param triggerId
+	 * @return status fetched from Nyx
+	 * @throws TriggerManagerException
+	 */
+	public static boolean isNyxTriggerReady(Long triggerId)
+			throws TriggerManagerException {
+		if (triggerId == -1) {
+			throw new TriggerManagerException("Trigger is not registered");
+		}
 
-    try {
-      ExecutorApiClient client = ExecutorApiClient.getInstance();
-      URI uri = ExecutorApiClient.buildUri(nyxServiceHost, port, "/status/" + triggerId, isHttp);
-      String response = client.httpGet(uri, null);
-      Map<String, Object> parsedResponse = (Map<String, Object>) JSON.parse(response);
+		try {
+			ExecutorApiClient client = ExecutorApiClient.getInstance();
+			URI uri = ExecutorApiClient.buildUri(nyxServiceHost, port,
+					"/status/" + triggerId, isHttp);
+			String response = client.httpGet(uri, null);
+			Map<String, Object> parsedResponse = (Map<String, Object>) JSON
+					.parse(response);
 
-      if (parsedResponse.containsKey("error")) {
-        throw new IllegalArgumentException((String) parsedResponse.get("error"));
-      } else if (parsedResponse.containsKey("ready")) {
-        return (boolean) parsedResponse.get("ready");
-      } else {
-        throw new Exception("Status missing from Nyx response :" + response);
-      }
-    } catch (Exception ex) {
-      logger.error("Failed to get Nyx service response for triggerId : " + triggerId, ex);
-      throw new TriggerManagerException(ex);
-    }
-  }
+			if (parsedResponse.containsKey("error")) {
+				throw new IllegalArgumentException(
+						(String) parsedResponse.get("error"));
+			} else if (parsedResponse.containsKey("ready")) {
+				return (boolean) parsedResponse.get("ready");
+			} else {
+				throw new Exception("Status missing from Nyx response :"
+						+ response);
+			}
+		} catch (Exception ex) {
+			logger.error("Failed to get Nyx service response for triggerId : "
+					+ triggerId, ex);
+			throw new TriggerManagerException(ex);
+		}
+	}
 
-  /**
-   * Fetches the trigger status details.
-   *
-   * @param triggerId
-   * @return the detailed status in Map<String Object> type.
-   * @throws TriggerManagerException
-   * */
-  @SuppressWarnings("unchecked")
-  public static Map<String, Object> getNyxTriggerStatus(Long triggerId) throws TriggerManagerException {
-    if (triggerId == -1) {
-      throw new TriggerManagerException("Trigger is not registered");
-    }
+	/**
+	 * Fetches the trigger status details.
+	 *
+	 * @param triggerId
+	 * @return the detailed status in Map<String Object> type.
+	 * @throws TriggerManagerException
+	 * */
+	@SuppressWarnings("unchecked")
+	public static Map<String, Object> getNyxTriggerStatus(Long triggerId)
+			throws TriggerManagerException {
+		if (triggerId == -1) {
+			throw new TriggerManagerException("Trigger is not registered");
+		}
 
-    try {
-      ExecutorApiClient client = ExecutorApiClient.getInstance();
-      URI uri = ExecutorApiClient.buildUri(nyxServiceHost, port, "/trigger/" + triggerId, isHttp);
-      String response = client.httpGet(uri, null);
-      return (Map<String, Object>) JSON.parse(response);
-    } catch (Exception ex) {
-      logger.error("Failed to get Nyx service response for triggerId : " + triggerId, ex);
-      throw new TriggerManagerException(ex);
-    }
-  }
+		try {
+			ExecutorApiClient client = ExecutorApiClient.getInstance();
+			URI uri = ExecutorApiClient.buildUri(nyxServiceHost, port,
+					"/trigger/" + triggerId, isHttp);
+			String response = client.httpGet(uri, null);
+			return (Map<String, Object>) JSON.parse(response);
+		} catch (Exception ex) {
+			logger.error("Failed to get Nyx service response for triggerId : "
+					+ triggerId, ex);
+			throw new TriggerManagerException(ex);
+		}
+	}
 }
--- azkaban-webserver/src/web/js/azkaban/view/schedule-trigger.js
+++ azkaban-webserver/src/web/js/azkaban/view/schedule-trigger.js
@@ -16,104 +16,125 @@
 
 $.namespace('azkaban');
 
-azkaban.ViewTriggerStatusView = Backbone.View.extend({
-  events : {
-    "click" : "closeEditingTarget",
-  },
+azkaban.ViewTriggerStatusView = Backbone.View
+		.extend({
+			events : {
+				"click" : "closeEditingTarget",
+			},
 
-  initialize : function(setting) {
+			initialize : function(setting) {
+			},
 
-  },
+			initFromSched : function(scheduleId) {
+				this.scheduleId = scheduleId;
+				var scheduleURL = contextURL + "/schedule"
+				this.scheduleURL = scheduleURL;
 
-  initFromSched : function(scheduleId) {
+				var fetchTriggerData = {
+					"scheduleId" : this.scheduleId,
+					"ajax" : "fetchNyxTriggerStatus"
+				};
 
-    this.scheduleId = scheduleId;
-    var scheduleURL = contextURL + "/schedule"
-    this.scheduleURL = scheduleURL;
+				var showDialog = function(title, message) {
+					$('#messageTitle').text(title);
+					$('#messageBox').text(message);
+					$('#messageDialog').modal();
+				}
 
-    var fetchTriggerData = {
-      "scheduleId" : this.scheduleId,
-      "ajax" : "fetchNyxTriggerStatus"
-    };
+				// main function to handle the data communication.
+				// Note : "NyxTriggerChecker_1" is the name of the nyx checker
+				// defined in the servlet.
+				// we need this value to locate the result in the result bag, if
+				// the value changed from the
+				// server side we need to make the corresponding changes here as
+				// well.
+				// returning result.
+				var successHandler = function(data) {
+					// only process if the expected value is returned in the
+					// data bag.
+					if (data.NyxTriggerChecker_1 != undefined
+							&& data.NyxTriggerChecker_1 != null
+							&& data.NyxTriggerChecker_1.hasOwnProperty("id")) {
 
-    var showDialog = function(title, message) {
-      $('#messageTitle').text(title);
-      $('#messageBox').text(message);
-      $('#messageDialog').modal();
-    }
+						var trigger = data.NyxTriggerChecker_1;
+						var ruleRel = "unknown";
+						var triggerActive = trigger.active ? "<span class='label label-success'>Active</span>"
+								: "<span class='label label-danger'>Inactive</span>";
+						var triggerReady = trigger.ready ? "<span class='label label-success'>Ready</span>"
+								: "<span class='label label-danger'>NotReady</span>";
+						var triggerId = trigger.id ? trigger.id : "unknown";
+						var triggerName = trigger.name ? trigger.name
+								: "unknown";
+						var triggerOwner = trigger.owner ? trigger.owner
+								: "unknown";
+						var triggerCreatedTM = trigger.createdTs ? (new Date(
+								trigger.createdTs)).toString() : "unknown";
+						var triggerUpdatedTM = trigger.updatedTs ? (new Date(
+								trigger.updatedTs)).toString() : "unknown";
 
-    // main function to handle the data communication.
-    // Note : "NyxTriggerChecker_1" is the name of the nyx checker defined in the servlet.
-    // we need this value to locate the result in the result bag, if the value changed from the
-    // server side we need to make the corresponding changes here as well.
-    // returning result.
-    var successHandler = function(data) {
-      // only process if the expected value is returned in the data bag.
-      if (data.NyxTriggerChecker_1 != undefined && data.NyxTriggerChecker_1 != null) {
+						if (trigger.ruleSet != undefined
+								&& trigger.ruleSet != null) {
+							// retrieve the rule relation from the expression
+							// field.
+							ruleRel = trigger.ruleSet.expression ? trigger.ruleSet.expression
+									: ruleRel;
 
-        var trigger = data.NyxTriggerChecker_1;
-        var ruleRel = "unknown";
-        var triggerActive = trigger.active ? "<span class='label label-success'>Active</span>"
-            : "<span class='label label-danger'>Inactive</span>";
-        var triggerReady = trigger.ready ? "<span class='label label-success'>Ready</span>"
-            : "<span class='label label-danger'>NotReady</span>";
-        var triggerName = trigger.name ? trigger.name : "unknown";
-        var triggerOwner = trigger.owner ? trigger.owner : "unknown";
-        var triggerCreatedTM = trigger.createdTs ? (new Date(trigger.createdTs)).toString() : "unknown";
-        var triggerUpdatedTM = trigger.updatedTs ? (new Date(trigger.updatedTs)).toString() : "unknown";
+							// fill in the list for the rules.
+							if (trigger.ruleSet.ruleList != undefined
+									&& trigger.ruleSet.ruleList != null) {
+								// clear the tbody each time the dialog is
+								// rendered.
+								$('#triggerStatusTbl tbody tr').remove();
 
-        if (trigger.ruleSet != undefined && trigger.ruleSet != null) {
-          // retrieve the rule relation from the expression field.
-          ruleRel = trigger.ruleSet.expression ? trigger.ruleSet.expression : ruleRel;
+								var ruleList = trigger.ruleSet.ruleList;
+								var ruleTbl = $('#triggerStatusTbl').get(0).tBodies[0];
 
-          // fill in the list for the rules.
-          if (trigger.ruleSet.ruleList != undefined && trigger.ruleSet.ruleList != null) {
-            // clear the tbody each time the dialog is rendered.
-            $('#triggerStatusTbl tbody tr').remove();
+								// fill in the rule info. data to render :
+								// - name , type , status , Update time
+								for ( var idx in ruleList) {
+									var row = ruleTbl.insertRow();
+									row.insertCell().innerText = ruleList[idx].name;
+									row.insertCell().innerText = ruleList[idx].ruleType;
+									row.insertCell().innerHTML = ruleList[idx].ready ? "<span class='label label-success'>Ready</span>"
+											: "<span class='label label-danger'>NotReady</span>";
+									row.insertCell().innerText = ruleList[idx].updatedTs ? (new Date(
+											ruleList[idx].updatedTs))
+											.toString()
+											: "unknown";
+								}
+							}
+						}
 
-            var ruleList = trigger.ruleSet.ruleList;
-            var ruleTbl = $('#triggerStatusTbl').get(0).tBodies[0];
+						// fill the place holders on UI.
+						$('#triggerId').text(triggerId);
+						$('#triggerName').text(triggerName);
+						$('#triggerOwner').text(triggerOwner);
+						$('#triggerActive').html(triggerActive);
+						$('#triggerReady').html(triggerReady);
+						$('#triggerCreatedTM').text(triggerCreatedTM);
+						$('#triggerUpdatedTM').text(triggerUpdatedTM);
+						$('#ruleRel').text(ruleRel);
+						$('#rawData').val(JSON.stringify(trigger, null, 8));
 
-            // fill in the rule info. data to render :
-            // - name , type , status , Update time
-            for ( var idx in ruleList) {
-              var row = ruleTbl.insertRow();
-              row.insertCell().innerText = ruleList[idx].name;
-              row.insertCell().innerText = ruleList[idx].ruleType;
-              row.insertCell().innerHTML = ruleList[idx].ready ? "<span class='label label-success'>Ready</span>"
-                  : "<span class='label label-danger'>NotReady</span>";
-              row.insertCell().innerText = ruleList[idx].updatedTs ? (new Date(ruleList[idx].updatedTs)).toString()
-                  : "unknown";
-            }
-          }
-        }
+						// modal the UI after all the info is successfully
+						// received.
+						$('#trigger-status').modal();
 
-        // fill the place holders on UI.
-        $('#triggerName').text(triggerName);
-        $('#triggerOwner').text(triggerOwner);
-        $('#triggerActive').html(triggerActive);
-        $('#triggerReady').html(triggerReady);
-        $('#triggerCreatedTM').text(triggerCreatedTM);
-        $('#triggerUpdatedTM').text(triggerUpdatedTM);
-        $('#ruleRel').text(ruleRel);
-        $('#rawData').val(JSON.stringify(trigger, null, 8));
+						console.log("Loaded trigger info.");
+					} else {
+						showDialog(
+								"Trigger Status Unavaiable",
+								"Unable to get trigger status,"
+										+ " trigger is either not yet registered with the triggering serivce or the triggering service is not returning a valid status data.");
+						return;
+					}
+				};
 
-        // modal the UI after all the info is successfully received.
-        $('#trigger-status').modal();
+				$.get(this.scheduleURL, fetchTriggerData, successHandler,
+						"json");
+			},
 
-        console.log("Loaded trigger info.");
-      } else {
-        showDialog("Unable to get trigger info", "Unable to get trigger info for the schedule,"
-            + " the schduled flow either doesn't have a trigger defined or the trigger service is unavailable."
-            + " (NYXTRIGGERCHECKER_1_NOT_FOUND)");
-        return;
-      }
-    };
+			closeEditingTarget : function() {
+			}
 
-    $.get(this.scheduleURL, fetchTriggerData, successHandler, "json");
-  },
-
-  closeEditingTarget : function() {
-  }
-
-});
+		});
