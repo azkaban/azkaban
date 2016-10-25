3dd27d2ee7217ca2260f6c622deab0f232f970c9
latang <latang@linkedin.com>
2016-10-24 21:13:32 -0700

    disable checking execute user override in job configuration

--- azkaban-common/src/main/java/azkaban/jobExecutor/ProcessJob.java
+++ azkaban-common/src/main/java/azkaban/jobExecutor/ProcessJob.java
@@ -176,12 +176,7 @@ public class ProcessJob extends AbstractProcessJob {
   }
 
   private boolean determineExecuteAsUser(Props sysProps, Props jobProps) {
-    boolean isExecuteAsUser = sysProps.getBoolean(EXECUTE_AS_USER, false);
-    // putting an override in case user needs to override. A temporary opening
-    if (jobProps.containsKey(EXECUTE_AS_USER_OVERRIDE))
-      isExecuteAsUser = jobProps.getBoolean(EXECUTE_AS_USER_OVERRIDE, false);
-
-    return isExecuteAsUser;
+    return sysProps.getBoolean(EXECUTE_AS_USER, false);
   }
 
   /**
@@ -189,10 +184,10 @@ public class ProcessJob extends AbstractProcessJob {
    * This method extracts the kerberos ticket cache file name from the jobprops.
    * This method will ensure that each job execution will have its own kerberos ticket cache file
    * Given that the code only sets an environmental variable, the number of files created corresponds
-   * to the number of processes that are doing kinit in their flow, which should not be an inordinately 
+   * to the number of processes that are doing kinit in their flow, which should not be an inordinately
    * high number.
    * </pre>
-   * 
+   *
    * @return file name: the kerberos ticket cache file to use
    */
   private String getKrb5ccname(Props jobProps) {
@@ -218,7 +213,7 @@ public class ProcessJob extends AbstractProcessJob {
    * 1. USER_TO_PROXY
    * 2. SUBMIT_USER
    * </pre>
-   * 
+   *
    * @param jobProps
    * @return the user that Azkaban is going to execute as
    */
