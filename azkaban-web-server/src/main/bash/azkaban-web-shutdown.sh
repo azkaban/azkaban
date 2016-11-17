e70ec07b426976532f57d277d3ff123bf54dd751
Suvodeep Pyne <spyne@linkedin.com>
2016-11-17 11:35:54 -0800

    Refactored shutdown scripts
    The executor server dumps the current server port to executor.port file. On shutdown clean it up.
    Also remove process ID files which are created upon launch.

--- azkaban-web-server/src/main/bash/azkaban-web-shutdown.sh
+++ azkaban-web-server/src/main/bash/azkaban-web-shutdown.sh
@@ -1,8 +1,15 @@
-azkaban_dir=$(dirname $0)/..
+#!/usr/bin/env bash
+# Shutdown script for azkaban web server
+installdir="$(dirname $0)/.."
 
-#!/bin/bash
-proc=`cat $azkaban_dir/currentpid`
-echo "killing AzkabanWebServer"
-kill $proc
+pid=`cat ${installdir}/currentpid`
+echo "Killing Web Server. [pid: $pid]"
 
-cat /dev/null > $azkaban_dir/currentpid
+kill ${pid}
+if [ $? -ne 0 ]; then
+    echo "Error: Shutdown failed"
+    exit 1;
+fi
+
+rm  ${installdir}/currentpid
+echo "done."
