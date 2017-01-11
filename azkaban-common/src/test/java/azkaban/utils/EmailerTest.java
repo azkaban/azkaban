5966487670ee9d71c6099e9bed32b38fd03c9bfc
qiaoshi.wang <qiaoshi.wang@fraudmetrix.cn>
2017-01-11 15:54:03 +0800

    Comple EmailerTest and  AbstractMailerTest

--- azkaban-common/src/test/java/azkaban/utils/EmailerTest.java
+++ azkaban-common/src/test/java/azkaban/utils/EmailerTest.java
@@ -1,18 +1,3 @@
-/*
- * Copyright 2014 LinkedIn Corp.
- *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not
- * use this file except in compliance with the License. You may obtain a copy of
- * the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations under
- * the License.
- */
 package azkaban.utils;
 
 import org.junit.Test;
@@ -20,18 +5,20 @@ import org.junit.Test;
 import java.util.ArrayList;
 import java.util.List;
 
-public class AbstractMailerTest {
+/**
+ * Created by wqs on 17/1/11.
+ */
+public class EmailerTest {
+
 
     /**
      * test emailMessage properties
      */
     @Test
     public void testCreateEmailMessage(){
-
         List<String> senderList = new ArrayList<String>();
         senderList.add("sender@xxx.com");
         EmailMessage emailMessage = null;
-
 
 
         //for Custom
@@ -41,8 +28,10 @@ public class AbstractMailerTest {
         props.put("mail.password","pwd");
         props.put("mail.sender","somebody@xxx.com");
         props.put("server.port","114");
+        props.put("jetty.use.ssl","false");
         props.put("server.useSSL","false");
-        AbstractMailer mailer = new AbstractMailer(props);
+        props.put("jetty.port","8786");
+        AbstractMailer mailer = new Emailer(props);
         emailMessage = mailer.createEmailMessage("subject","text/html",senderList);
 
         assert emailMessage.getMailPort()==445;
@@ -55,7 +44,9 @@ public class AbstractMailerTest {
         defaultProps.put("mail.sender","somebody@xxx.com");
         defaultProps.put("server.port","114");
         defaultProps.put("server.useSSL","false");
-        mailer = new AbstractMailer(defaultProps);
+        defaultProps.put("server.useSSL","true");
+        defaultProps.put("jetty.ssl.port","8080");
+        mailer = new Emailer(defaultProps);
         emailMessage = mailer.createEmailMessage("subject","text/html",senderList);
 
         assert emailMessage.getMailPort()==25;
