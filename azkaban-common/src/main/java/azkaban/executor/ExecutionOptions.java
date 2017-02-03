5176f5f4696ca6222138553e9e184d0f3dbd410e
wyukawa <wyukawa@gmail.com>
2017-02-03 11:40:23 +0900

    set concurrent option to CONCURRENT_OPTION_SKIP

--- azkaban-common/src/main/java/azkaban/executor/ExecutionOptions.java
+++ azkaban-common/src/main/java/azkaban/executor/ExecutionOptions.java
@@ -64,7 +64,7 @@ public class ExecutionOptions {
   private Integer pipelineLevel = null;
   private Integer pipelineExecId = null;
   private Integer queueLevel = 0;
-  private String concurrentOption = CONCURRENT_OPTION_IGNORE;
+  private String concurrentOption = CONCURRENT_OPTION_SKIP;
   private String mailCreator = DefaultMailCreator.DEFAULT_MAIL_CREATOR;
   private boolean memoryCheck = true;
   private Map<String, String> flowParameters = new HashMap<String, String>();
