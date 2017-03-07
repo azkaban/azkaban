2b30e7dd8bc8c3a9b29200a8dfa60ddda7228215
Liang Tang <liang.xjtu@gmail.com>
2017-03-07 09:46:16 -0800

    Metrics Code Refactor and adding key metrics (#930)
    * Metrics Code Refactor and adding key metrics
    
    This commit adds and fixes the following metrics:
    * Flow failure rate
    * project zip uploading time duration log
    * correct the right NumRunningFlows in web server
    
    * add Jetty thread pool metrics as a Web Server performance monitor
    
    * adding measure DB connection time as a metrics for DB connection pool performance

--- azkaban-common/src/main/java/azkaban/executor/ExecutorManager.java
+++ azkaban-common/src/main/java/azkaban/executor/ExecutorManager.java
@@ -16,6 +16,7 @@
 
 package azkaban.executor;
 
+import azkaban.metrics.CommonMetrics;
 import azkaban.constants.ServerProperties;
 import azkaban.utils.FlowUtils;
 import java.io.File;
@@ -1537,6 +1538,10 @@ public class ExecutorManager extends EventHandler implements
     flow.applyUpdateObject(updateData);
     Status newStatus = flow.getStatus();
 
+    if(oldStatus != newStatus && newStatus == Status.FAILED) {
+      CommonMetrics.INSTANCE.markFlowFail();
+    }
+
     ExecutionOptions options = flow.getExecutionOptions();
     if (oldStatus != newStatus && newStatus.equals(Status.FAILED_FINISHING)) {
       // We want to see if we should give an email status on first failure.
@@ -1572,12 +1577,12 @@ public class ExecutorManager extends EventHandler implements
 
   public boolean isFinished(ExecutableFlow flow) {
     switch (flow.getStatus()) {
-    case SUCCEEDED:
-    case FAILED:
-    case KILLED:
-      return true;
-    default:
-      return false;
+      case SUCCEEDED:
+      case FAILED:
+      case KILLED:
+        return true;
+      default:
+        return false;
     }
   }
 
