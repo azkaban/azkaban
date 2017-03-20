dd8b07f9ac8d01ad11e9504b12124e722db95d73
Suvodeep Pyne <spyne@linkedin.com>
2017-03-20 16:52:51 -0700

    Managing Dependencies for projects
    Each project will be curated using a ProjectDependencyManager instance which work with DependencyFetcher to fetch dependencies.
    Added tests.

---
+++ azkaban-common/src/main/java/azkaban/project/ProjectManagerConfig.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2017 LinkedIn Corp.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ *
+ */
+
+package azkaban.project;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+
+/**
+ * Configuration class for ProjectManager.
+ *
+ * All configurable parameters can be stored here. This class is intended to be passed around.
+ */
+public class ProjectManagerConfig {
+  private Path fetchTmpDir = Paths.get("fetch");
+
+  public Path getFetchTmpDir() {
+    return fetchTmpDir;
+  }
+
+  public void setFetchTmpDir(Path fetchTmpDir) {
+    this.fetchTmpDir = fetchTmpDir;
+  }
+}
