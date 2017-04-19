dffcdbbb0bd8a03af6d26a3d312716ada0efbde5
Suvodeep Pyne <spyne@linkedin.com>
2017-04-18 17:58:40 -0700

    Added uploadProject template to StorageManager

--- azkaban-common/src/main/java/azkaban/storage/StorageManager.java
+++ azkaban-common/src/main/java/azkaban/storage/StorageManager.java
@@ -17,11 +17,25 @@
 
 package azkaban.storage;
 
+import azkaban.project.Project;
 import azkaban.spi.Storage;
+import azkaban.spi.StorageException;
+import azkaban.spi.StorageMetadata;
 import com.google.inject.Inject;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.net.URI;
+import org.apache.log4j.Logger;
 
 
+/**
+ * StorageManager manages and coordinates all interactions with the Storage layer. This also includes bookkeeping
+ * like updating DB with the new versionm, etc
+ */
 public class StorageManager {
+  private static final Logger log = Logger.getLogger(StorageManager.class);
 
   private final Storage storage;
 
@@ -30,7 +44,44 @@ public class StorageManager {
     this.storage = storage;
   }
 
-  public void uploadProject(String projectId) {
+  /**
+   * API to a project file into Azkaban Storage
+   *
+   * @param project           project ID
+   * @param fileExtension     extension of the file
+   * @param filename          name of the file
+   * @param localFile         local file
+   * @param uploader          the user who uploaded
+   */
+  public void uploadProject(
+      Project project,
+      String fileExtension,
+      String filename,
+      File localFile,
+      String uploader) {
+    final StorageMetadata metadata = new StorageMetadata(
+        String.valueOf(project.getId()),
+        String.valueOf(getLatestVersion(project)),
+        fileExtension
+    );
+    log.info(String.format(
+        "Uploading project. Uploader: %s, Metadata:%s, filename: %s[%d bytes]",
+        uploader, metadata, filename, localFile.length()
+    ));
+    try {
+      uploadProject(metadata, new FileInputStream(localFile));
+    } catch (FileNotFoundException e) {
+      throw new StorageException(e);
+    }
+  }
 
+  private int getLatestVersion(Project project) {
+    // TODO Implement
+    return -1;
+  }
+
+  public void uploadProject(StorageMetadata metadata, InputStream is) {
+    // TODO Implement
+    URI key = storage.put(metadata, is);
   }
 }
