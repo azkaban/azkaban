49f711e8ec80aa4021fd2618c334fbf693f05700
Liang Tang <liang.xjtu@gmail.com>
2017-04-28 14:46:22 -0700

    move sql files from azkaban-sql to azkaban-db
    This patch consolidate the azkaban-sql and azkaban-db modules into one place. Basically we move sql files from azkaban-sql into `azkaban-db/src/main/sql`. Tested by `gradle build`. Also, these files is able to appear in the right place (Distribution zip) after `gradle installDist`.

--- azkaban-sql/src/sql/create.active_executing_flows.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE active_executing_flows (
-	exec_id INT,
-	update_time BIGINT,
-	PRIMARY KEY (exec_id)
-);
--- azkaban-sql/src/sql/create.properties.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE properties (
-	name VARCHAR(64) NOT NULL,
-	type INT NOT NULL,
-	modified_time BIGINT NOT NULL,
-	value VARCHAR(256),
-	PRIMARY KEY (name, type)
-);
