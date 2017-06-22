azkaban/azkaban

commit 7c52eda672d6845a02e6811c6081963f9d775132

EndDate Expiring Schedule Introduction (#1110)
* Implementing End Date feature to AZ schedules

Today, when we specify a flow schedule in Azkaban, the flow will run infinitely without termination as per the schedule. As #721 proposes, should schedule have an end date, the flow will have more flexibilities to be managed. In this PR, I implement the end date feature to allow users to specify the expiration date given a schedule. UI has not been changed yet in this patch (will not be implemented in near future), but people will be able to call API to leverage the new feature.

The API command sample:
`curl -k -d ajax=scheduleCronFlow -d projectName=wtwt -d flow=azkaban -d endSchedTime=14XXXXXXXXXX --data-urlencode cronExpression="0 * * ? * 6" -b "azkaban.browser.session.id=XXXXXXXXXXXXXX" http://localhost:8081/schedule`

I reuse BasicTimeChecker, as part of Expire Condition, in order to not break Old Azkaban flows. Also, Added one parameter `endSchedTime` to schedule API. If the users don't specify this parameter, Azkaban would use the default value for `01/01/2050`, and the schedule will be almost running infinitely. Besides, I add Unit tests as much as possible to guarantee that this change will not break out.

azkaban-common/src/main/java/azkaban/scheduler/TriggerBasedScheduleLoader.java
@@ -16,6 +16,7 @@
 
 package azkaban.scheduler;
 
+import azkaban.Constants;
 import azkaban.trigger.Condition;
 import azkaban.trigger.ConditionChecker;
 import azkaban.trigger.Trigger;
@@ -43,7 +44,7 @@ public class TriggerBasedScheduleLoader implements ScheduleLoader {
   private long lastUpdateTime = -1;
 
   public TriggerBasedScheduleLoader(final TriggerManager triggerManager,
-      final String triggerSource) {
+                                    final String triggerSource) {
     this.triggerManager = triggerManager;
     this.triggerSource = triggerSource;
   }
@@ -95,18 +96,14 @@ public class TriggerBasedScheduleLoader implements ScheduleLoader {
     return cond;
   }
 
-  // if failed to trigger, auto expire?
   private Condition createExpireCondition(final Schedule s) {
-    final Map<String, ConditionChecker> checkers =
-        new HashMap<>();
-    final ConditionChecker checker =
-        new BasicTimeChecker("BasicTimeChecker_2", s.getFirstSchedTime(),
-            s.getTimezone(), s.isRecurring(), s.skipPastOccurrences(),
-            s.getPeriod(), s.getCronExpression());
+    final Map<String, ConditionChecker> checkers = new HashMap<>();
+    final ConditionChecker checker = new BasicTimeChecker("EndTimeChecker_1", s.getFirstSchedTime(),
+        s.getTimezone(), s.getEndSchedTime(), false, false,
+        null, null);
     checkers.put(checker.getId(), checker);
     final String expr = checker.getId() + ".eval()";
-    final Condition cond = new Condition(checkers, expr);
-    return cond;
+    return new Condition(checkers, expr);
   }
 
   @Override
@@ -130,33 +127,11 @@ public class TriggerBasedScheduleLoader implements ScheduleLoader {
     }
   }
 
-  // TODO may need to add logic to filter out skip runs
-  @Override
-  public synchronized List<Schedule> loadSchedules()
-      throws ScheduleManagerException {
-    final List<Trigger> triggers = this.triggerManager.getTriggers(this.triggerSource);
-    final List<Schedule> schedules = new ArrayList<>();
-    for (final Trigger t : triggers) {
-      this.lastUpdateTime = Math.max(this.lastUpdateTime, t.getLastModifyTime());
-      final Schedule s = triggerToSchedule(t);
-      schedules.add(s);
-      System.out.println("loaded schedule for "
-          + s.getProjectName() + " (project_ID: " + s.getProjectId() + ")");
-    }
-    return schedules;
-
-  }
-
   private Schedule triggerToSchedule(final Trigger t) throws ScheduleManagerException {
-    final Condition triggerCond = t.getTriggerCondition();
-    final Map<String, ConditionChecker> checkers = triggerCond.getCheckers();
-    BasicTimeChecker ck = null;
-    for (final ConditionChecker checker : checkers.values()) {
-      if (checker.getType().equals(BasicTimeChecker.type)) {
-        ck = (BasicTimeChecker) checker;
-        break;
-      }
-    }
+
+    final BasicTimeChecker triggerTimeChecker = getBasicTimeChecker(t.getTriggerCondition().getCheckers());
+    final BasicTimeChecker endTimeChecker = getBasicTimeChecker(t.getExpireCondition().getCheckers());
+
     final List<TriggerAction> actions = t.getActions();
     ExecuteFlowAction act = null;
     for (final TriggerAction action : actions) {
@@ -165,20 +140,38 @@ public class TriggerBasedScheduleLoader implements ScheduleLoader {
         break;
       }
     }
-    if (ck != null && act != null) {
-      final Schedule s =
-          new Schedule(t.getTriggerId(), act.getProjectId(),
-              act.getProjectName(), act.getFlowName(),
-              t.getStatus().toString(), ck.getFirstCheckTime(),
-              ck.getTimeZone(), ck.getPeriod(), t.getLastModifyTime(),
-              ck.getNextCheckTime(), t.getSubmitTime(), t.getSubmitUser(),
-              act.getExecutionOptions(), act.getSlaOptions(), ck.getCronExpression());
-      return s;
+    if (triggerTimeChecker != null && act != null) {
+      return new Schedule(t.getTriggerId(),
+          act.getProjectId(),
+          act.getProjectName(),
+          act.getFlowName(),
+          t.getStatus().toString(),
+          triggerTimeChecker.getFirstCheckTime(),
+          endTimeChecker == null? Constants.DEFAULT_SCHEDULE_END_EPOCH_TIME: endTimeChecker.getNextCheckTime(),
+          triggerTimeChecker.getTimeZone(),
+          triggerTimeChecker.getPeriod(),
+          t.getLastModifyTime(),
+          triggerTimeChecker.getNextCheckTime(),
+          t.getSubmitTime(),
+          t.getSubmitUser(),
+          act.getExecutionOptions(),
+          act.getSlaOptions(),
+          triggerTimeChecker.getCronExpression());
     } else {
       logger.error("Failed to parse schedule from trigger!");
       throw new ScheduleManagerException(
           "Failed to parse schedule from trigger!");
     }
+  }
+
+  // expirecheckers or triggerCheckers only have BasicTimeChecker today. This should be refactored in future.
+  private BasicTimeChecker getBasicTimeChecker(final Map<String, ConditionChecker> checkers) {
+    for (final ConditionChecker checker : checkers.values()) {
+      if (checker.getType().equals(BasicTimeChecker.type)) {
+        return (BasicTimeChecker) checker;
+      }
+    }
+    return null;
   }
 
   @Override
@@ -212,7 +205,7 @@ public class TriggerBasedScheduleLoader implements ScheduleLoader {
       this.lastUpdateTime = Math.max(this.lastUpdateTime, t.getLastModifyTime());
       final Schedule s = triggerToSchedule(t);
       schedules.add(s);
-      System.out.println("loaded schedule for "
+      logger.info("loaded schedule for "
           + s.getProjectName() + " (project_ID: " + s.getProjectId() + ")");
     }
     return schedules;

azkaban-common/src/main/java/azkaban/Constants.java
@@ -49,6 +49,9 @@ public class Constants {
   public static final int DEFAULT_SSL_PORT_NUMBER = 8443;
   public static final int DEFAULT_JETTY_MAX_THREAD_COUNT = 20;
 
+  // One Schedule's default End Time: 01/01/2050, 00:00:00, UTC
+  public static final long DEFAULT_SCHEDULE_END_EPOCH_TIME = 2524608000000L;
+
   public static class ConfigurationKeys {
     // These properties are configurable through azkaban.properties
 

azkaban-common/src/main/java/azkaban/scheduler/Schedule.java
@@ -43,6 +43,7 @@ public class Schedule {
   private final String projectName;
   private final String flowName;
   private final long firstSchedTime;
+  private final long endSchedTime;
   private final DateTimeZone timezone;
   private final long lastModifyTime;
   private final ReadablePeriod period;
@@ -56,40 +57,28 @@ public class Schedule {
   private ExecutionOptions executionOptions;
   private List<SlaOption> slaOptions;
 
-  public Schedule(final int scheduleId, final int projectId, final String projectName,
-      final String flowName, final String status, final long firstSchedTime,
-      final DateTimeZone timezone, final ReadablePeriod period, final long lastModifyTime,
-      final long nextExecTime, final long submitTime, final String submitUser) {
-
-    this(scheduleId, projectId, projectName, flowName, status, firstSchedTime,
-        timezone, period, lastModifyTime, nextExecTime, submitTime, submitUser,
-        null, null, null);
-  }
-
-  public Schedule(final int scheduleId, final int projectId, final String projectName,
-      final String flowName, final String status, final long firstSchedTime,
-      final String timezoneId,
-      final String period, final long lastModifyTime, final long nextExecTime,
-      final long submitTime,
-      final String submitUser, final ExecutionOptions executionOptions,
-      final List<SlaOption> slaOptions) {
-    this(scheduleId, projectId, projectName, flowName, status, firstSchedTime,
-        DateTimeZone.forID(timezoneId), parsePeriodString(period),
-        lastModifyTime, nextExecTime, submitTime, submitUser, executionOptions,
-        slaOptions, null);
-  }
-
-  public Schedule(final int scheduleId, final int projectId, final String projectName,
-      final String flowName, final String status, final long firstSchedTime,
-      final DateTimeZone timezone, final ReadablePeriod period, final long lastModifyTime,
-      final long nextExecTime, final long submitTime, final String submitUser,
-      final ExecutionOptions executionOptions, final List<SlaOption> slaOptions,
-      final String cronExpression) {
+  public Schedule(final int scheduleId,
+                  final int projectId,
+                  final String projectName,
+                  final String flowName,
+                  final String status,
+                  final long firstSchedTime,
+                  final long endSchedTime,
+                  final DateTimeZone timezone,
+                  final ReadablePeriod period,
+                  final long lastModifyTime,
+                  final long nextExecTime,
+                  final long submitTime,
+                  final String submitUser,
+                  final ExecutionOptions executionOptions,
+                  final List<SlaOption> slaOptions,
+                  final String cronExpression) {
     this.scheduleId = scheduleId;
     this.projectId = projectId;
     this.projectName = projectName;
     this.flowName = flowName;
     this.firstSchedTime = firstSchedTime;
+    this.endSchedTime = endSchedTime;
     this.timezone = timezone;
     this.lastModifyTime = lastModifyTime;
     this.period = period;
@@ -112,27 +101,27 @@ public class Schedule {
     final int periodInt =
         Integer.parseInt(periodStr.substring(0, periodStr.length() - 1));
     switch (periodUnit) {
-      case 'M':
-        period = Months.months(periodInt);
-        break;
-      case 'w':
-        period = Weeks.weeks(periodInt);
-        break;
-      case 'd':
-        period = Days.days(periodInt);
-        break;
-      case 'h':
-        period = Hours.hours(periodInt);
-        break;
-      case 'm':
-        period = Minutes.minutes(periodInt);
-        break;
-      case 's':
-        period = Seconds.seconds(periodInt);
-        break;
-      default:
-        throw new IllegalArgumentException("Invalid schedule period unit '"
-            + periodUnit);
+    case 'M':
+      period = Months.months(periodInt);
+      break;
+    case 'w':
+      period = Weeks.weeks(periodInt);
+      break;
+    case 'd':
+      period = Days.days(periodInt);
+      break;
+    case 'h':
+      period = Hours.hours(periodInt);
+      break;
+    case 'm':
+      period = Minutes.minutes(periodInt);
+      break;
+    case 's':
+      period = Seconds.seconds(periodInt);
+      break;
+    default:
+      throw new IllegalArgumentException("Invalid schedule period unit '"
+          + periodUnit);
     }
 
     return period;
@@ -192,9 +181,7 @@ public class Schedule {
   public String toString() {
 
     final String underlying =
-        this.projectName + "." + this.flowName + " (" + this.projectId + ")"
-            + " to be run at (starting) "
-            + new DateTime(
+        this.projectName + "." + this.flowName + " (" + this.projectId + ")" + " to be run at (starting) " + new DateTime(
             this.firstSchedTime).toDateTimeISO();
     if (this.period == null && this.cronExpression == null) {
       return underlying + " non-recurring";
@@ -275,8 +262,9 @@ public class Schedule {
     }
 
     if (this.cronExpression != null) {
-      final DateTime nextTime = getNextCronRuntime(this.nextExecTime, this.timezone,
-          Utils.parseCronExpression(this.cronExpression, this.timezone));
+      final DateTime nextTime = getNextCronRuntime(
+          this.nextExecTime, this.timezone, Utils.parseCronExpression(this.cronExpression,
+              this.timezone));
       this.nextExecTime = nextTime.getMillis();
       return true;
     }
@@ -292,7 +280,7 @@ public class Schedule {
   }
 
   private DateTime getNextRuntime(final long scheduleTime, final DateTimeZone timezone,
-      final ReadablePeriod period) {
+                                  final ReadablePeriod period) {
     final DateTime now = new DateTime();
     DateTime date = new DateTime(scheduleTime).withZone(timezone);
     int count = 0;
@@ -315,13 +303,14 @@ public class Schedule {
   }
 
   /**
-   * @param scheduleTime represents the time when Schedule Servlet receives the Cron Schedule API
-   * call.
+   *
+   * @param scheduleTime represents the time when Schedule Servlet receives the Cron Schedule API call.
    * @param timezone is always UTC (after 3.1.0)
+   * @param ce
    * @return the First Scheduled DateTime to run this flow.
    */
   private DateTime getNextCronRuntime(final long scheduleTime, final DateTimeZone timezone,
-      final CronExpression ce) {
+                                      final CronExpression ce) {
 
     Date date = new DateTime(scheduleTime).withZone(timezone).toDate();
     if (ce != null) {
@@ -387,4 +376,7 @@ public class Schedule {
     return this.skipPastOccurrences;
   }
 
+  public long getEndSchedTime() {
+    return this.endSchedTime;
+  }
 }

azkaban-common/src/main/java/azkaban/scheduler/ScheduleLoader.java
@@ -24,8 +24,6 @@ public interface ScheduleLoader {
 
   public void updateSchedule(Schedule s) throws ScheduleManagerException;
 
-  public List<Schedule> loadSchedules() throws ScheduleManagerException;
-
   public void removeSchedule(Schedule s) throws ScheduleManagerException;
 
   public void updateNextExecTime(Schedule s) throws ScheduleManagerException;

azkaban-common/src/main/java/azkaban/scheduler/ScheduleManager.java
@@ -41,7 +41,6 @@ import org.joda.time.format.DateTimeFormatter;
  * TODO kunkun-tang: When new AZ quartz Scheduler comes, we will remove this class.
  */
 public class ScheduleManager implements TriggerAgent {
-
   public static final String triggerSource = "SimpleTimeTrigger";
   private static final Logger logger = Logger.getLogger(ScheduleManager.class);
   private final DateTimeFormatter _dateFormat = DateTimeFormat
@@ -56,6 +55,7 @@ public class ScheduleManager implements TriggerAgent {
   /**
    * Give the schedule manager a loader class that will properly load the
    * schedule.
+   *
    */
   public ScheduleManager(final ScheduleLoader loader) {
     this.loader = loader;
@@ -95,6 +95,7 @@ public class ScheduleManager implements TriggerAgent {
 
   /**
    * Retrieves a copy of the list of schedules.
+   *
    */
   public synchronized List<Schedule> getSchedules()
       throws ScheduleManagerException {
@@ -105,6 +106,7 @@ public class ScheduleManager implements TriggerAgent {
 
   /**
    * Returns the scheduled flow for the flow name
+   *
    */
   public Schedule getSchedule(final int projectId, final String flowId)
       throws ScheduleManagerException {
@@ -126,6 +128,7 @@ public class ScheduleManager implements TriggerAgent {
 
   /**
    * Removes the flow from the schedule if it exists.
+   *
    */
   public synchronized void removeSchedule(final Schedule sched) {
     final Pair<Integer, String> identityPairMap = sched.getScheduleIdentityPair();
@@ -145,43 +148,50 @@ public class ScheduleManager implements TriggerAgent {
   }
 
   public Schedule scheduleFlow(final int scheduleId,
-      final int projectId,
-      final String projectName,
-      final String flowName,
-      final String status,
-      final long firstSchedTime,
-      final DateTimeZone timezone,
-      final ReadablePeriod period,
-      final long lastModifyTime,
-      final long nextExecTime,
-      final long submitTime,
-      final String submitUser,
-      final ExecutionOptions execOptions,
-      final List<SlaOption> slaOptions) {
-    final Schedule sched =
-        new Schedule(scheduleId, projectId, projectName, flowName, status,
-            firstSchedTime, timezone, period, lastModifyTime, nextExecTime,
-            submitTime, submitUser, execOptions, slaOptions, null);
+                               final int projectId,
+                               final String projectName,
+                               final String flowName,
+                               final String status,
+                               final long firstSchedTime,
+                               final long endSchedTime,
+                               final DateTimeZone timezone,
+                               final ReadablePeriod period,
+                               final long lastModifyTime,
+                               final long nextExecTime,
+                               final long submitTime,
+                               final String submitUser,
+                               final ExecutionOptions execOptions,
+                               final List<SlaOption> slaOptions) {
+    final Schedule sched = new Schedule(scheduleId, projectId, projectName, flowName, status,
+        firstSchedTime, endSchedTime, timezone, period, lastModifyTime, nextExecTime,
+        submitTime, submitUser, execOptions, slaOptions, null);
     logger
         .info("Scheduling flow '" + sched.getScheduleName() + "' for "
-            + this._dateFormat.print(firstSchedTime) + " with a period of " + (period == null
-            ? "(non-recurring)"
+            + this._dateFormat.print(firstSchedTime) + " with a period of " + (period == null ? "(non-recurring)"
             : period));
 
     insertSchedule(sched);
     return sched;
   }
 
-  public Schedule cronScheduleFlow(final int scheduleId, final int projectId,
-      final String projectName, final String flowName, final String status,
-      final long firstSchedTime, final DateTimeZone timezone,
-      final long lastModifyTime,
-      final long nextExecTime, final long submitTime, final String submitUser,
-      final ExecutionOptions execOptions, final List<SlaOption> slaOptions,
-      final String cronExpression) {
+  public Schedule cronScheduleFlow(final int scheduleId,
+                                   final int projectId,
+                                   final String projectName,
+                                   final String flowName,
+                                   final String status,
+                                   final long firstSchedTime,
+                                   final long endSchedTime,
+                                   final DateTimeZone timezone,
+                                   final long lastModifyTime,
+                                   final long nextExecTime,
+                                   final long submitTime,
+                                   final String submitUser,
+                                   final ExecutionOptions execOptions,
+                                   final List<SlaOption> slaOptions,
+                                   final String cronExpression) {
     final Schedule sched =
         new Schedule(scheduleId, projectId, projectName, flowName, status,
-            firstSchedTime, timezone, null, lastModifyTime, nextExecTime,
+            firstSchedTime, endSchedTime, timezone, null, lastModifyTime, nextExecTime,
             submitTime, submitUser, execOptions, slaOptions, cronExpression);
     logger
         .info("Scheduling flow '" + sched.getScheduleName() + "' for "
@@ -190,7 +200,6 @@ public class ScheduleManager implements TriggerAgent {
     insertSchedule(sched);
     return sched;
   }
-
   /**
    * Schedules the flow, but doesn't save the schedule afterwards.
    */

azkaban-common/src/main/java/azkaban/trigger/Condition.java
@@ -30,7 +30,7 @@ public class Condition {
 
   private static final Logger logger = Logger.getLogger(Condition.class);
 
-  private static JexlEngine jexl = new JexlEngine();
+  private static final JexlEngine jexl = new JexlEngine();
   private static CheckerTypeLoader checkerLoader = null;
   private final MapContext context = new MapContext();
   private Expression expression;
@@ -45,18 +45,10 @@ public class Condition {
   }
 
   public Condition(final Map<String, ConditionChecker> checkers, final String expr,
-      final long nextCheckTime) {
+                   final long nextCheckTime) {
     this.nextCheckTime = nextCheckTime;
     setCheckers(checkers);
     this.expression = jexl.createExpression(expr);
-  }
-
-  public synchronized static void setJexlEngine(final JexlEngine jexl) {
-    Condition.jexl = jexl;
-  }
-
-  protected static CheckerTypeLoader getCheckerLoader() {
-    return checkerLoader;
   }
 
   public synchronized static void setCheckerLoader(final CheckerTypeLoader loader) {
@@ -98,12 +90,6 @@ public class Condition {
     return cond;
   }
 
-  protected void registerChecker(final ConditionChecker checker) {
-    this.checkers.put(checker.getId(), checker);
-    this.context.set(checker.getId(), checker);
-    updateNextCheckTime();
-  }
-
   public long getNextCheckTime() {
     return this.nextCheckTime;
   }
@@ -112,18 +98,12 @@ public class Condition {
     return this.checkers;
   }
 
-  public void setCheckers(final Map<String, ConditionChecker> checkers) {
+  private void setCheckers(final Map<String, ConditionChecker> checkers) {
     this.checkers = checkers;
     for (final ConditionChecker checker : checkers.values()) {
       this.context.set(checker.getId(), checker);
     }
     updateNextCheckTime();
-  }
-
-  public void updateCheckTime(final Long ct) {
-    if (this.nextCheckTime < ct) {
-      this.nextCheckTime = ct;
-    }
   }
 
   private void updateNextCheckTime() {

commit 4bca20ad879f660476c30d3a0168af2abaae674e

fix endtime schedule (#1246)
An issue was introduced in #1110 that causing schedule end time is set to a past date whenever SLA changes are made.
It's caused by incompatibility of new code #1110 with old schedule metadata. SLA change triggers reloading updated schedules into memory. But instead of using a default schedule end time, which we set to some day in year 2050, it always mistakenly uses a past date as the expiring time of the schedule.
This fixes the issue by setting appropriate end time of the schedule.

azkaban-common/src/main/java/azkaban/scheduler/TriggerBasedScheduleLoader.java
@@ -44,7 +44,7 @@ public class TriggerBasedScheduleLoader implements ScheduleLoader {
   private long lastUpdateTime = -1;
 
   public TriggerBasedScheduleLoader(final TriggerManager triggerManager,
-                                    final String triggerSource) {
+      final String triggerSource) {
     this.triggerManager = triggerManager;
     this.triggerSource = triggerSource;
   }
@@ -129,8 +129,9 @@ public class TriggerBasedScheduleLoader implements ScheduleLoader {
 
   private Schedule triggerToSchedule(final Trigger t) throws ScheduleManagerException {
 
-    final BasicTimeChecker triggerTimeChecker = getBasicTimeChecker(t.getTriggerCondition().getCheckers());
-    final BasicTimeChecker endTimeChecker = getBasicTimeChecker(t.getExpireCondition().getCheckers());
+    final BasicTimeChecker triggerTimeChecker = getBasicTimeChecker(
+        t.getTriggerCondition().getCheckers());
+    final BasicTimeChecker endTimeChecker = getEndTimeChecker(t);
 
     final List<TriggerAction> actions = t.getActions();
     ExecuteFlowAction act = null;
@@ -147,7 +148,8 @@ public class TriggerBasedScheduleLoader implements ScheduleLoader {
           act.getFlowName(),
           t.getStatus().toString(),
           triggerTimeChecker.getFirstCheckTime(),
-          endTimeChecker == null? Constants.DEFAULT_SCHEDULE_END_EPOCH_TIME: endTimeChecker.getNextCheckTime(),
+          endTimeChecker == null ? Constants.DEFAULT_SCHEDULE_END_EPOCH_TIME
+              : endTimeChecker.getNextCheckTime(),
           triggerTimeChecker.getTimeZone(),
           triggerTimeChecker.getPeriod(),
           t.getLastModifyTime(),
@@ -173,6 +175,14 @@ public class TriggerBasedScheduleLoader implements ScheduleLoader {
     }
     return null;
   }
+
+  private BasicTimeChecker getEndTimeChecker(final Trigger t) {
+    if (t.getExpireCondition().getExpression().contains("EndTimeChecker")) {
+      return getBasicTimeChecker(t.getExpireCondition().getCheckers());
+    }
+    return null;
+  }
+
 
   @Override
   public void removeSchedule(final Schedule s) throws ScheduleManagerException {