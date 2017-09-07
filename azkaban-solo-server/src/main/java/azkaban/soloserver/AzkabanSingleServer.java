f86031e92b1772df1e6b77ba4b24d97d1373bccf
juhoautio <autiojuho@gmail.com>
2017-09-07 11:40:09 +0300

    Write all files under local/

--- azkaban-solo-server/src/main/java/azkaban/soloserver/AzkabanSingleServer.java
+++ azkaban-solo-server/src/main/java/azkaban/soloserver/AzkabanSingleServer.java
@@ -88,12 +88,12 @@ public class AzkabanSingleServer {
    */
   private static String[] prepareDefaultConf() throws IOException {
     final File templateFolder = new File("test/local-conf-templates");
-    final File localConfFolder = new File("local");
+    final File localConfFolder = new File("local/conf");
     if (!localConfFolder.exists()) {
-      FileUtils.copyDirectory(templateFolder, localConfFolder);
+      FileUtils.copyDirectory(templateFolder, localConfFolder.getParentFile());
       log.info("Copied local conf templates from " + templateFolder.getAbsolutePath());
     }
-    log.info("Using conf at " + localConfFolder.getAbsolutePath() + "/conf");
+    log.info("Using conf at " + localConfFolder.getAbsolutePath());
     return new String[]{"-conf", "local/conf"};
   }
 
