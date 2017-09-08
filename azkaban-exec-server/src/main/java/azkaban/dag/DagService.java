e5b6348873673169ba49e50a93bfbe70f6847e39
Ray Yang <ruiguo@gmail.com>
2017-09-07 19:45:56 -0700

    Add shutdown DagService method

--- azkaban-exec-server/src/main/java/azkaban/dag/DagService.java
+++ azkaban-exec-server/src/main/java/azkaban/dag/DagService.java
@@ -18,7 +18,10 @@ package azkaban.dag;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 import javax.inject.Singleton;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Thread safe and non blocking service for DAG processing.
@@ -27,6 +30,9 @@ import javax.inject.Singleton;
  */
 @Singleton
 public class DagService {
+
+  private static final long SHUTDOWN_WAIT_TIMEOUT = 60;
+  private static final Logger logger = LoggerFactory.getLogger(DagService.class);
 
   private final ExecutorService executorService = Executors.newSingleThreadExecutor();
 
@@ -48,5 +54,33 @@ public class DagService {
 
   public void unblockJob(final Node node) {
 
+  }
+
+  /**
+   * Shuts down the service and wait for the tasks to finish.
+   *
+   * Adopted from
+   *
+   * <a href="https://docs.oracle.com/javase/7/docs/api/java/util/concurrent/ExecutorService.html">Java
+   * Docs
+   * </a>
+   */
+  public void shutdownAndAwaitTermination() {
+    this.executorService.shutdown(); // Disable new tasks from being submitted
+    try {
+      // Wait a while for existing tasks to terminate
+      if (!this.executorService.awaitTermination(SHUTDOWN_WAIT_TIMEOUT, TimeUnit.SECONDS)) {
+        this.executorService.shutdownNow(); // Cancel currently executing tasks
+        // Wait a while for tasks to respond to being cancelled
+        if (!this.executorService.awaitTermination(SHUTDOWN_WAIT_TIMEOUT, TimeUnit.SECONDS)) {
+          logger.error("The DagService did not terminate.");
+        }
+      }
+    } catch (final InterruptedException ie) {
+      // (Re-)Cancel if current thread also interrupted
+      this.executorService.shutdownNow();
+      // Preserve interrupt status
+      Thread.currentThread().interrupt();
+    }
   }
 }
