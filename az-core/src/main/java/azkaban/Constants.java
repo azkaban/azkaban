26025cf9d7c0b1acac70d3006565476bfd0e6b6c
Cheng Ren <rencheng311@gmail.com>
2017-09-28 17:36:47 -0700

    new FlowTrigger and Dependency class (#1482)
    FlowTrigger and Dependency are two immutable classes holding the trigger/dependency configuration and parameters. They will be constructed from trigger config files. Once created, they couldn't be modified.
    
    Class constructors will also perform following validations:
    
    check all required parameters are there.
    no duplicate dependency name across same trigger's dependencies.
    no duplicate dependency parameters across same trigger's dependencies(different name same other parameters).

--- az-core/src/main/java/azkaban/Constants.java
+++ az-core/src/main/java/azkaban/Constants.java
@@ -89,7 +89,7 @@ public class Constants {
 
     // User facing web server configurations used to construct the user facing server URLs. They are useful when there is a reverse proxy between Azkaban web servers and users.
     // enduser -> myazkabanhost:443 -> proxy -> localhost:8081
-    // when this parameters set then these parameters are used to generate email links. 
+    // when this parameters set then these parameters are used to generate email links.
     // if these parameters are not set then jetty.hostname, and jetty.port(if ssl configured jetty.ssl.port) are used.
     public static final String AZKABAN_WEBSERVER_EXTERNAL_HOSTNAME = "azkaban.webserver.external_hostname";
     public static final String AZKABAN_WEBSERVER_EXTERNAL_SSL_PORT = "azkaban.webserver.external_ssl_port";
---
+++ azkaban-common/src/main/java/azkaban/project/FlowTriggerDependency.java
@@ -0,0 +1,71 @@
+/*
+ * Copyright 2017 LinkedIn Corp.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package azkaban.project;
+
+import com.google.common.base.Preconditions;
+import java.util.Collections;
+import java.util.Map;
+import org.apache.commons.lang.StringUtils;
+
+/**
+ * FlowTriggerDependency is the logic representation of a trigger dependency.
+ * It couldn't be changed once gets constructed.
+ * It will be used to create running dependency instance.
+ */
+public class FlowTriggerDependency {
+
+  private final Map<String, String> props;
+  private final String name;
+  private final String type;
+
+  /**
+   * @throws IllegalArgumentException if name or type is null or blank
+   * @throws IllegalArgumentException if depProps is null
+   */
+  public FlowTriggerDependency(final String name, final String type, final Map<String, String>
+      depProps) {
+    Preconditions.checkArgument(StringUtils.isNotBlank(name));
+    Preconditions.checkArgument(StringUtils.isNotBlank(type));
+    Preconditions.checkArgument(depProps != null);
+    this.name = name;
+    this.type = type;
+    this.props = Collections.unmodifiableMap(depProps);
+    //todo chengren311: validate per dependencyType: some dependency type might need extra special
+    //check, also check if it's a valid dependency type
+  }
+
+  public String getName() {
+    return this.name;
+  }
+
+  public String getType() {
+    return this.type;
+  }
+
+  public Map<String, String> getProps() {
+    return this.props;
+  }
+
+  @Override
+  public String toString() {
+    return "FlowTriggerDependency{" +
+        "props=" + this.props +
+        ", name='" + this.name + '\'' +
+        ", type='" + this.type + '\'' +
+        '}';
+  }
+}
