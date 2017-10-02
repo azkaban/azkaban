5ca4c150a50c826fc20b74937d9001458bfd2fac
Liang Tang <liang.xjtu@gmail.com>
2017-10-02 13:50:51 -0700

    adding unit guice tests

--- azkaban-common/src/test/java/azkaban/ServiceProviderTest.java
+++ azkaban-common/src/test/java/azkaban/ServiceProviderTest.java
@@ -19,14 +19,17 @@ package azkaban;
 
 import static azkaban.ServiceProvider.SERVICE_PROVIDER;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import azkaban.db.DatabaseOperator;
 import azkaban.project.JdbcProjectImpl;
 import azkaban.spi.Storage;
 import azkaban.storage.DatabaseStorage;
+import azkaban.storage.HdfsStorage;
 import azkaban.storage.LocalStorage;
 import azkaban.storage.StorageManager;
 import azkaban.utils.Props;
+import com.google.inject.ConfigurationException;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
 import java.io.File;
@@ -37,7 +40,9 @@ import org.junit.Test;
 
 public class ServiceProviderTest {
 
-  public static final String AZKABAN_LOCAL_TEST_STORAGE = "AZKABAN_LOCAL_TEST_STORAGE";
+  private static final String AZKABAN_LOCAL_TEST_STORAGE = "AZKABAN_LOCAL_TEST_STORAGE";
+  private static final String AZKABAN_TEST_HDFS_STORAGE_TYPE = "HDFS";
+  private static final String AZKABAN_TEST_STORAGE_HDFS_URI= "hdfs://test.com:9000/azkaban/";
 
   // Test if one class is singletonly guiced. could be called by
   // AZ Common, Web, or Exec Modules.
@@ -71,7 +76,34 @@ public class ServiceProviderTest {
     assertThat(injector.getInstance(StorageManager.class)).isNotNull();
     assertThat(injector.getInstance(DatabaseStorage.class)).isNotNull();
     assertThat(injector.getInstance(LocalStorage.class)).isNotNull();
+    assertThatThrownBy(
+        () -> injector.getInstance(HdfsStorage.class))
+        .isInstanceOf(ConfigurationException.class)
+        .hasMessageContaining("Guice configuration errors");
+
     assertThat(injector.getInstance(Storage.class)).isNotNull();
     assertThat(injector.getInstance(DatabaseOperator.class)).isNotNull();
+  }
+
+  @Test
+  public void testHadoopInjection() throws Exception {
+    final Props props = new Props();
+    props.put("database.type", "h2");
+    props.put("h2.path", "h2");
+    props
+        .put(Constants.ConfigurationKeys.AZKABAN_STORAGE_TYPE, AZKABAN_TEST_HDFS_STORAGE_TYPE);
+    props
+        .put(Constants.ConfigurationKeys.HADOOP_CONF_DIR_PATH, "./");
+    props .put(Constants.ConfigurationKeys.AZKABAN_STORAGE_HDFS_ROOT_URI,
+        AZKABAN_TEST_STORAGE_HDFS_URI);
+
+    final Injector injector = Guice.createInjector(
+        new AzkabanCommonModule(props)
+    );
+    SERVICE_PROVIDER.unsetInjector();
+    SERVICE_PROVIDER.setInjector(injector);
+
+    assertSingleton(HdfsStorage.class, injector);
+    assertThat(injector.getInstance(Storage.class)).isNotNull();
   }
 }
