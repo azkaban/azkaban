ad1c9445c9fd602b26438a86b1b05bb0fbc5e5f2
Liang Tang <liang.xjtu@gmail.com>
2017-10-23 17:06:01 -0700

    commit 1

---
+++ azkaban-hadoop-security-plugin/src/main/java/azkaban/security/SampleCredential.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2017 LinkedIn Corp.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ *
+ */
+
+package azkaban.security;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Random;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.security.Credentials;
+
+/**
+ * This class is going to be moved to a separate MP.
+ */
+public class SampleCredential implements Credential {
+
+  private static final Text SECRET_KEY_NAME = new Text("azkaban.datavault.key");
+  private final Credentials hadoopCrednetial;
+
+  public SampleCredential(final Credentials hadoopCrednetial) {
+    this.hadoopCrednetial = hadoopCrednetial;
+  }
+
+  @Override
+  public void register(final String user) {
+    this.hadoopCrednetial.addSecretKey(SECRET_KEY_NAME, getSecretKey(user));
+  }
+
+  private byte[] getSecretKey(final String user) {
+    // would call out to service
+    final Random r = new Random();
+    return (String.valueOf(r.nextInt(10000)) + user).getBytes(StandardCharsets.UTF_8);
+  }
+}
