13f02f0b2af01a6a3b42a0516ec335d28e274af0
juho-autio <juho.autio@rovio.com>
2018-03-23 12:53:06 +0200

    Test to reproduce 413 Payload Too Large
    Related to #1653 "Azkaban web evicts running executions if there are too many".

---
+++ azkaban-common/src/test/java/azkaban/executor/ExecutorApiGatewaySystemTest.java
@@ -0,0 +1,60 @@
+package azkaban.executor;
+
+import azkaban.utils.JSONUtils;
+import azkaban.utils.Pair;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+
+@Ignore("Requires azkaban server running -> run AzkabanSingleServer first")
+public class ExecutorApiGatewaySystemTest {
+
+  private ExecutorApiGateway apiGateway;
+
+  @Before
+  public void setUp() throws Exception {
+    ExecutorApiClient client = new ExecutorApiClient();
+    apiGateway = new ExecutorApiGateway(client);
+  }
+
+  @Test
+  public void update100Executions() throws Exception {
+    updateExecutions(100);
+  }
+
+  @Test
+  public void update300Executions() throws Exception {
+    // this fails because the URL is too long
+    updateExecutions(300);
+  }
+
+  private void updateExecutions(int count) throws ExecutorManagerException {
+    final List<Integer> executionIdsList = new ArrayList<>();
+    final List<Long> updateTimesList = new ArrayList<>();
+
+    for (int i = 100000; i < 100000 + count; i++) {
+      executionIdsList.add(i);
+      updateTimesList.add(0L);
+    }
+
+    final Pair<String, String> executionIds =
+        new Pair<>(ConnectorParams.EXEC_ID_LIST_PARAM,
+            JSONUtils.toJSON(executionIdsList));
+
+    final Pair<String, String> updateTimes =
+        new Pair<>(
+            ConnectorParams.UPDATE_TIME_LIST_PARAM,
+            JSONUtils.toJSON(updateTimesList));
+
+    Map<String, Object> results = apiGateway.callWithExecutionId("localhost", 12321,
+        ConnectorParams.UPDATE_ACTION, null, null, executionIds, updateTimes);
+
+    Assert.assertTrue(results != null && !results.isEmpty());
+  }
+
+}
