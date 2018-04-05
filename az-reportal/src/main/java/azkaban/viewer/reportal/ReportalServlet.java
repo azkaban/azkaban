1c0260584b7c18c9c3b989c54916aa7bd5bb2009
Liang Tang <liang.xjtu@gmail.com>
2018-04-05 10:09:55 -0700

    proxy user

--- az-reportal/src/main/java/azkaban/viewer/reportal/ReportalServlet.java
+++ az-reportal/src/main/java/azkaban/viewer/reportal/ReportalServlet.java
@@ -900,6 +900,15 @@ public class ReportalServlet extends LoginAbstractAzkabanServlet {
         proxyUser = variable.name;
       }
 
+      if (variable.title.equals("reportal.config.user.to.proxy")) {
+        String userToProxy = variable.name;
+        final UserManager userManager = getApplication().getUserManager();
+        if (!userManager.validateProxyUser(userToProxy, user)) {
+          errors.add("User " + user.getUserId() + " has no permission to add " + userToProxy
+              + " as proxy user.");
+        }
+      }
+
       variableList.add(variable);
     }
 
