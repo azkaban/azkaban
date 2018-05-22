5ee55ca0cbbf153868172b4889d513288a025aac
HappyRay <ruiguo@gmail.com>
2018-05-21 17:26:09 -0700

    Improve tests for Status (#1766)
    Even though we have 100% line coverage, the logical coverage is more
    limited. This change improves that.

--- azkaban-exec-server/src/test/java/azkaban/dag/StatusTest.java
+++ azkaban-exec-server/src/test/java/azkaban/dag/StatusTest.java
@@ -18,29 +18,85 @@ package azkaban.dag;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.HashMap;
+import java.util.Map;
 import org.junit.Test;
 
 public class StatusTest {
 
-  //todo HappyRay: add tests for other status
   @Test
-  public void disabled_is_terminal() {
-    assertThat(Status.DISABLED.isTerminal()).isTrue();
+  public void is_terminal() {
+    // given
+    final Map<Status, Boolean> isStatusTerminalMap = new HashMap<>();
+    isStatusTerminalMap.put(Status.BLOCKED, Boolean.FALSE);
+    isStatusTerminalMap.put(Status.CANCELED, Boolean.TRUE);
+    isStatusTerminalMap.put(Status.DISABLED, Boolean.TRUE);
+    isStatusTerminalMap.put(Status.FAILURE, Boolean.TRUE);
+    isStatusTerminalMap.put(Status.KILLED, Boolean.TRUE);
+    isStatusTerminalMap.put(Status.KILLING, Boolean.FALSE);
+    isStatusTerminalMap.put(Status.READY, Boolean.FALSE);
+    isStatusTerminalMap.put(Status.RUNNING, Boolean.FALSE);
+    isStatusTerminalMap.put(Status.SUCCESS, Boolean.TRUE);
+
+    assertMapSizeMatchEnumSize(isStatusTerminalMap);
+    for (final Map.Entry<Status, Boolean> entry : isStatusTerminalMap.entrySet()) {
+      final Status key = entry.getKey();
+      assertThat(key.isTerminal()).as("key: %s", key).isEqualTo(entry.getValue());
+    }
   }
 
   @Test
-  public void running_is_not_terminal() {
-    assertThat(Status.RUNNING.isTerminal()).isFalse();
+  public void is_effectively_success() {
+    // given
+    final Map<Status, Boolean> isStatusEffectivelySuccess = new HashMap<>();
+    isStatusEffectivelySuccess.put(Status.BLOCKED, Boolean.FALSE);
+    isStatusEffectivelySuccess.put(Status.CANCELED, Boolean.FALSE);
+    isStatusEffectivelySuccess.put(Status.DISABLED, Boolean.TRUE);
+    isStatusEffectivelySuccess.put(Status.FAILURE, Boolean.FALSE);
+    isStatusEffectivelySuccess.put(Status.KILLED, Boolean.FALSE);
+    isStatusEffectivelySuccess.put(Status.KILLING, Boolean.FALSE);
+    isStatusEffectivelySuccess.put(Status.READY, Boolean.FALSE);
+    isStatusEffectivelySuccess.put(Status.RUNNING, Boolean.FALSE);
+    isStatusEffectivelySuccess.put(Status.SUCCESS, Boolean.TRUE);
+
+    assertMapSizeMatchEnumSize(isStatusEffectivelySuccess);
+    for (final Map.Entry<Status, Boolean> entry : isStatusEffectivelySuccess.entrySet()) {
+      final Status key = entry.getKey();
+      assertThat(key.isSuccessEffectively()).as("key: %s", key).isEqualTo(entry.getValue());
+    }
   }
 
   @Test
-  public void diabled_is_effectively_success() {
-    assertThat(Status.DISABLED.isSuccessEffectively()).isTrue();
+  public void is_pre_run_state() {
+    // given
+    final Map<Status, Boolean> isPrerunStateMap = new HashMap<>();
+    isPrerunStateMap.put(Status.BLOCKED, Boolean.TRUE);
+    isPrerunStateMap.put(Status.CANCELED, Boolean.FALSE);
+    isPrerunStateMap.put(Status.DISABLED, Boolean.TRUE);
+    isPrerunStateMap.put(Status.FAILURE, Boolean.FALSE);
+    isPrerunStateMap.put(Status.KILLED, Boolean.FALSE);
+    isPrerunStateMap.put(Status.KILLING, Boolean.FALSE);
+    isPrerunStateMap.put(Status.READY, Boolean.TRUE);
+    isPrerunStateMap.put(Status.RUNNING, Boolean.FALSE);
+    isPrerunStateMap.put(Status.SUCCESS, Boolean.FALSE);
+
+    assertMapSizeMatchEnumSize(isPrerunStateMap);
+    for (final Map.Entry<Status, Boolean> entry : isPrerunStateMap.entrySet()) {
+      final Status key = entry.getKey();
+      assertThat(key.isPreRunState()).as("key: %s", key).isEqualTo(entry.getValue());
+    }
   }
 
-  @Test
-  public void failure_is_not_effectively_success() {
-    assertThat(Status.FAILURE.isSuccessEffectively()).isFalse();
+  /**
+   * Asserts the given map contains the same number of entries as the number of values of the {@link
+   * Status} has.
+   *
+   * @param map a map that contains status and its associated boolean value
+   */
+  private void assertMapSizeMatchEnumSize(
+      final Map<Status, Boolean> map) {
+    final int mapSize = map.size();
+    final int enumSize = Status.values().length;
+    assertThat(enumSize).isEqualTo(mapSize);
   }
-
 }
