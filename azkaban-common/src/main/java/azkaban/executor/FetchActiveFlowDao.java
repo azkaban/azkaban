10664b8479e7304f831a68fbcf64e94b72863286
juhoautio <autiojuho@gmail.com>
2018-06-29 23:35:58 +0300

    Remove unused method fetchActiveFlowByExecId

--- azkaban-common/src/main/java/azkaban/executor/FetchActiveFlowDao.java
+++ azkaban-common/src/main/java/azkaban/executor/FetchActiveFlowDao.java
@@ -23,20 +23,16 @@ import azkaban.utils.Pair;
 import java.io.IOException;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import javax.inject.Inject;
 import javax.inject.Singleton;
 import org.apache.commons.dbutils.ResultSetHandler;
-import org.apache.log4j.Logger;
 
 @Singleton
 public class FetchActiveFlowDao {
 
-  private static final Logger logger = Logger.getLogger(FetchActiveFlowDao.class);
   private final DatabaseOperator dbOperator;
 
   @Inject
@@ -51,23 +47,6 @@ public class FetchActiveFlowDao {
           new FetchActiveExecutableFlows());
     } catch (final SQLException e) {
       throw new ExecutorManagerException("Error fetching active flows", e);
-    }
-  }
-
-  Pair<ExecutionReference, ExecutableFlow> fetchActiveFlowByExecId(final int execId)
-      throws ExecutorManagerException {
-    try {
-      final List<Pair<ExecutionReference, ExecutableFlow>> flows =
-          this.dbOperator
-              .query(FetchActiveExecutableFlowByExecId.FETCH_ACTIVE_EXECUTABLE_FLOW_BY_EXECID,
-                  new FetchActiveExecutableFlowByExecId(), execId);
-      if (flows.isEmpty()) {
-        return null;
-      } else {
-        return flows.get(0);
-      }
-    } catch (final SQLException e) {
-      throw new ExecutorManagerException("Error fetching active flows by exec id", e);
     }
   }
 
@@ -126,57 +105,4 @@ public class FetchActiveFlowDao {
     }
   }
 
-  private static class FetchActiveExecutableFlowByExecId implements
-      ResultSetHandler<List<Pair<ExecutionReference, ExecutableFlow>>> {
-
-    private static final String FETCH_ACTIVE_EXECUTABLE_FLOW_BY_EXECID =
-        "SELECT ex.exec_id exec_id, ex.enc_type enc_type, ex.flow_data flow_data, et.host host, "
-            + "et.port port, et.id executorId, et.active executorStatus"
-            + " FROM execution_flows ex"
-            + " INNER JOIN "
-            + " executors et ON ex.executor_id = et.id"
-            + " Where ex.exec_id = ? AND ex.status NOT IN ("
-            + Status.SUCCEEDED.getNumVal() + ", "
-            + Status.KILLED.getNumVal() + ", "
-            + Status.FAILED.getNumVal() + ")";
-
-    @Override
-    public List<Pair<ExecutionReference, ExecutableFlow>> handle(final ResultSet rs)
-        throws SQLException {
-      if (!rs.next()) {
-        return Collections.emptyList();
-      }
-
-      final List<Pair<ExecutionReference, ExecutableFlow>> execFlows =
-          new ArrayList<>();
-      do {
-        final int id = rs.getInt(1);
-        final int encodingType = rs.getInt(2);
-        final byte[] data = rs.getBytes(3);
-        final String host = rs.getString(4);
-        final int port = rs.getInt(5);
-        final int executorId = rs.getInt(6);
-        final boolean executorStatus = rs.getBoolean(7);
-
-        if (data == null) {
-          logger.error("Found a flow with empty data blob exec_id: " + id);
-        } else {
-          final EncodingType encType = EncodingType.fromInteger(encodingType);
-          try {
-            final ExecutableFlow exFlow =
-                ExecutableFlow.createExecutableFlowFromObject(
-                    GZIPUtils.transformBytesToObject(data, encType));
-
-            final Executor executor = new Executor(executorId, host, port, executorStatus);
-            final ExecutionReference ref = new ExecutionReference(id, executor);
-            execFlows.add(new Pair<>(ref, exFlow));
-          } catch (final IOException e) {
-            throw new SQLException("Error retrieving flow data " + id, e);
-          }
-        }
-      } while (rs.next());
-
-      return execFlows;
-    }
-  }
 }
