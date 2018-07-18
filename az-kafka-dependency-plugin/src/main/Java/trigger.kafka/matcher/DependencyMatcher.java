9f1c32a2d36041f22ba5052bde52990faf632f0a
Chiawei Chang <hunkywei@gmail.com>
2018-07-18 16:41:07 -0700

    refactor into flow-plugin

--- az-kafka-dependency-plugin/src/main/Java/trigger.kafka/matcher/DependencyMatcher.java
+++
@@ -1,8 +0,0 @@
-package trigger.kafka.matcher;
-
-public interface DependencyMatcher {
-  /**
-   * Determine whether the dependency condition is match.
-   */
-  public boolean isMatch(String payload, String rule);
-}
