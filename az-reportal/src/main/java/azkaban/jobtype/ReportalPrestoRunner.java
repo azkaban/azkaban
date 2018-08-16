a0728a1733ea7211ef5f224532af0ad3cb090f4e
Cheng Ren <rencheng311@gmail.com>
2018-08-16 16:23:25 -0700

    in progress

--- az-reportal/src/main/java/azkaban/jobtype/ReportalPrestoRunner.java
+++ az-reportal/src/main/java/azkaban/jobtype/ReportalPrestoRunner.java
@@ -18,40 +18,27 @@ package azkaban.jobtype;
 
 import azkaban.crypto.Decryptions;
 import azkaban.reportal.util.ReportalUtil;
-import java.io.File;
 import java.io.IOException;
 import java.net.URI;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Properties;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileSystem;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class ReportalPrestoRunner extends ReportalAbstractRunner {
 
-  private static final Logger logger = LoggerFactory.getLogger(ReportalPrestoRunner.class);
+  private static final String PRESTO_DRIVER_PROP_PREFIX = "presto.driver.";
   private static final String IMPERSONATED_USER_KEY = "presto.execute.user";
+  private static final String JDBC_DRIVER_KEY = "presto.driver";
 
 
   public ReportalPrestoRunner(final String jobName, final Properties props) {
     super(props);
   }
-
-  static String filesToURIString(final File[] files) throws IOException {
-    final StringBuffer sb = new StringBuffer();
-    for (int i = 0; i < files.length; i++) {
-      sb.append("file:///").append(files[i].getCanonicalPath());
-      if (i != files.length - 1) {
-        sb.append(",");
-      }
-    }
-
-    return sb.toString();
-  }
-
 
   private String decrypt(final String encrypted, final String keyPath) throws IOException {
     final FileSystem fs = FileSystem.get(URI.create("file:///"), new Configuration());
@@ -61,25 +48,28 @@ public class ReportalPrestoRunner extends ReportalAbstractRunner {
 
   private Properties getProperties() throws IOException {
     final Properties connProperties = new Properties();
-    connProperties.put("jdbc.crypto.key.path", this.props.get("jdbc.crypto.key.path"));
+    final Map<String, String> prestoProps = this.props.getMapByPrefix(PRESTO_DRIVER_PROP_PREFIX);
 
-    connProperties.put("user", this.props.get("user"));
-
-    connProperties.put("password", decrypt(this.props.get("encrypted.password"),
-        this.props.get("jdbc.crypto.key.path")));
-
-    connProperties.put("SSLTrustStorePassword",
-        decrypt(this.props.get("encrypted.SSLTrustStorePassword"),
-            this.props.get("jdbc.crypto.key.path")));
-    connProperties
-        .put("SSLTrustStorePath", this.props.get("SSLTrustStorePath"));
+    for (final Entry<String, String> entry : prestoProps.entrySet()) {
+      final String key = entry.getKey();
+      final String value = entry.getValue();
+      if (key.contains("encrypted.")) {
+        // if props contains "encrypted." then decrypted it with key file and put the decrypted
+        // value into jdbc connection props
+        //"encrypted.password" => "password"
+        final String jdbcProp = key.replaceFirst("encrypted.", "");
+        connProperties.put(jdbcProp, decrypt(value, prestoProps.get("jdbc.crypto.key.path")));
+      } else {
+        connProperties.put(key, value);
+      }
+    }
 
     return connProperties;
   }
 
-  public Connection getConnection(final String jdbcUrl, final String userToProxy) {
+  private Connection getConnection(final String jdbcUrl, final String userToProxy) {
     try {
-      Class.forName("com.linkedin.daptor.jdbc.DaptorDriver");
+      Class.forName(this.props.get(JDBC_DRIVER_KEY));
       final Properties connProperties = getProperties();
       connProperties.put(IMPERSONATED_USER_KEY, userToProxy);
       final Connection conn = DriverManager.getConnection(jdbcUrl, connProperties);
@@ -91,8 +81,8 @@ public class ReportalPrestoRunner extends ReportalAbstractRunner {
 
   @Override
   protected void runReportal() throws Exception {
-
-    final Connection conn = getConnection(this.props.get("jdbc.url"), this.proxyUser);
+    final Connection conn = getConnection(this.props.get(PRESTO_DRIVER_PROP_PREFIX + "jdbc.url"),
+        this.proxyUser);
     final PreparedStatement statement = conn.prepareStatement(this.jobQuery);
     try {
       statement.execute();
