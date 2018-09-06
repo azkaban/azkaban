7a8ba29d8affe90773a7d1b1ce45fa28b7dbdd52
jamiesjc <jasun@linkein.com>
2018-09-06 13:07:06 -0700

    Disable flaky test: testKillBasicFlowWithoutEndNode

--- azkaban-exec-server/src/test/java/azkaban/execapp/FlowRunnerYamlTest.java
+++ azkaban-exec-server/src/test/java/azkaban/execapp/FlowRunnerYamlTest.java
@@ -26,6 +26,7 @@ import azkaban.project.Project;
 import azkaban.test.executions.ExecutionsTestUtil;
 import java.io.File;
 import java.util.HashMap;
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class FlowRunnerYamlTest extends FlowRunnerTestBase {
@@ -56,6 +57,7 @@ public class FlowRunnerYamlTest extends FlowRunnerTestBase {
     assertFlowStatus(flow, Status.SUCCEEDED);
   }
 
+  @Ignore
   @Test
   public void testKillBasicFlowWithoutEndNode() throws Exception {
     setUp(BASIC_FLOW_YAML_DIR, BASIC_FLOW_YAML_FILE);
