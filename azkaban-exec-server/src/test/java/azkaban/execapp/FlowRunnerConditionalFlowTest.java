2fa79cc1ef566401104c130e41573f95c85f0ea9
jamiesjc <jasun@linkein.com>
2018-09-14 11:17:25 -0700

    Fix flaky test runFlowOnJobStatusConditionNull_2

--- azkaban-exec-server/src/test/java/azkaban/execapp/FlowRunnerConditionalFlowTest.java
+++ azkaban-exec-server/src/test/java/azkaban/execapp/FlowRunnerConditionalFlowTest.java
@@ -147,7 +147,9 @@ public class FlowRunnerConditionalFlowTest extends FlowRunnerTestBase {
     setUp(CONDITIONAL_FLOW_3, flowProps);
     final ExecutableFlow flow = this.runner.getExecutableFlow();
     flow.getExecutableNode("jobC").setConditionOnJobStatus(null);
+    InteractiveTestJob.getTestJob("jobA").succeedJob();
     assertStatus(flow, "jobA", Status.SUCCEEDED);
+    InteractiveTestJob.getTestJob("jobB").succeedJob();
     assertStatus(flow, "jobB", Status.SUCCEEDED);
     assertStatus(flow, "jobC", Status.SUCCEEDED);
     assertFlowStatus(flow, Status.SUCCEEDED);
