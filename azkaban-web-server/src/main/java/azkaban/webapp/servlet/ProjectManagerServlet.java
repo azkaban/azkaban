fbc6aea818983f4606e15021a4fb2b6ea22dbf47
Cheng Ren <rencheng311@gmail.com>
2018-09-14 17:17:36 -0700

    add proper error message when project is not in database (#1948)

--- azkaban-web-server/src/main/java/azkaban/webapp/servlet/ProjectManagerServlet.java
+++ azkaban-web-server/src/main/java/azkaban/webapp/servlet/ProjectManagerServlet.java
@@ -1671,11 +1671,13 @@ public class ProjectManagerServlet extends LoginAbstractAzkabanServlet {
     final User user = session.getUser();
     final String projectName = (String) multipart.get("project");
     final Project project = this.projectManager.getProject(projectName);
-    if (!project.isActive()) {
-      registerError(ret, "Installation Failed. Project '" + project.getName()
-          + "' was already removed.", resp, 410);
+    if(project == null || !project.isActive()) {
+      String failureCause = project == null ? "doesn't exist." : "was already removed.";
+      registerError(ret, "Installation Failed. Project '" + projectName + " "
+          + failureCause, resp, 410);
       return;
     }
+
     logger.info(
         "Upload: reference of project " + projectName + " is " + System.identityHashCode(project));
 
