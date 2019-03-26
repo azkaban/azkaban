5c461d18073d775db34f0854a94bb1b968fd9bcb
Toby Allsopp <toby.allsopp@wherescape.com>
2019-03-26 18:07:46 +1300

    FlowRunnerYamlTest.testAlertOnFlowFinished: Wait for deterministic flow status
    About once every 200 executions on my machine this test was failing due to
    the flow being killed instead of failed because jobs A and B were still in
    the running state when the flow finished. Waiting for the flow to catch up
    with their failures before letting job C succeed (hopefully) eliminates
    this possibility.

--- azkaban-exec-server/src/test/java/azkaban/execapp/FlowRunnerYamlTest.java
+++ azkaban-exec-server/src/test/java/azkaban/execapp/FlowRunnerYamlTest.java
@@ -1,18 +1,18 @@
 /*
-* Copyright 2018 LinkedIn Corp.
-*
-* Licensed under the Apache License, Version 2.0 (the “License”); you may not
-* use this file except in compliance with the License. You may obtain a copy of
-* the License at
-*
-* http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an “AS IS” BASIS, WITHOUT
-* WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
-* License for the specific language governing permissions and limitations under
-* the License.
-*/
+ * Copyright 2018 LinkedIn Corp.
+ *
+ * Licensed under the Apache License, Version 2.0 (the “License”); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an “AS IS” BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
 package azkaban.execapp;
 
 import static org.mockito.ArgumentMatchers.any;
@@ -136,7 +136,9 @@ public class FlowRunnerYamlTest extends FlowRunnerTestBase {
     when(this.runner.getAlerterHolder().get("email")).thenReturn(mailAlerter);
     FlowRunnerTestUtil.startThread(this.runner);
     InteractiveTestJob.getTestJob("jobA").failJob();
+    assertFlowStatus(flow, Status.FAILED_FINISHING);
     InteractiveTestJob.getTestJob("jobB").failJob();
+    assertFlowStatus(flow, Status.FAILED_FINISHING);
     InteractiveTestJob.getTestJob("jobC").succeedJob();
     assertFlowStatus(flow, Status.FAILED);
     verify(mailAlerter).alertOnError(flow, "Flow finished");
