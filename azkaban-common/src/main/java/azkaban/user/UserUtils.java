2af73f287ada9de442994ca89bbee23a7ac6d318
Deepak Jaiswal <djaiswal@djaiswal-mn1.linkedin.biz>
2019-04-04 22:21:12 -0700

    Proper implementation to handle multiple config files in a single setupWatch call. Also implemented review comments.

--- azkaban-common/src/main/java/azkaban/user/UserUtils.java
+++ azkaban-common/src/main/java/azkaban/user/UserUtils.java
@@ -1,6 +1,8 @@
 package azkaban.user;
 
 import com.google.common.base.Preconditions;
+import com.google.common.collect.HashMultimap;
+import com.google.common.collect.Multimap;
 import com.sun.nio.file.SensitivityWatchEventModifier;
 import java.io.File;
 import java.io.IOException;
@@ -11,6 +13,7 @@ import java.nio.file.StandardWatchEventKinds;
 import java.nio.file.WatchEvent;
 import java.nio.file.WatchKey;
 import java.nio.file.WatchService;
+import java.util.HashMap;
 import java.util.Map;
 import org.apache.log4j.Logger;
 
@@ -43,56 +46,89 @@ public final class UserUtils {
    * configurations
    */
   static void setupWatch(final Map<String, ParseConfigFile> configFileMap) {
-    Preconditions.checkArgument(configFileMap != null);
+    Preconditions.checkNotNull(configFileMap);
+    Preconditions.checkArgument(configFileMap.size() > 0);
 
     Runnable runnable = () -> {
-      WatchService watchService;
-      Path path;
+      WatchService watchService  = null;
+      // Map to store WatchKey to Dir mapping
+      final Map<WatchKey, Path> keys = new HashMap<>();
+      // A directory to config files multimap
+      final Multimap<Path, String> dirToFilesMap = HashMultimap.create();
+
+      // Iterate over each file.
       for (Map.Entry<String, ParseConfigFile> entry : configFileMap.entrySet()) {
         String fileName = entry.getKey();
         ParseConfigFile parser = entry.getValue();
-        Preconditions.checkArgument(fileName != null && parser != null);
+        Preconditions.checkNotNull(fileName);
+        Preconditions.checkNotNull(parser);
 
         final File file = new File(fileName);
         if (!file.exists()) {
-          throw new IllegalArgumentException("User provided file " + fileName + " does not exist.");
+          log.warn("Failed to reload config, user provided file " + fileName + " does not exist.");
+          continue;
         }
 
         try {
           watchService = FileSystems.getDefault().newWatchService();
-          path = Paths.get(fileName).getParent();
-          path.register(watchService, new WatchEvent.Kind[]{StandardWatchEventKinds.ENTRY_MODIFY},
-              SensitivityWatchEventModifier.HIGH);
+          Path dir = Paths.get(fileName).getParent();
+          if (!dirToFilesMap.containsKey(dir)) {
+            // There is not entry for this directory, create a watchkey
+            WatchKey watchKey = dir.register(watchService,
+                new WatchEvent.Kind[]{StandardWatchEventKinds.ENTRY_MODIFY},
+                SensitivityWatchEventModifier.HIGH);
+            keys.put(watchKey, dir);
+          }
+          // Add the config file to dir map
+          dirToFilesMap.put(dir, fileName);
         } catch (IOException e) {
           // Ignore the IOException
-          log.warn("IOException while setting up watch on conf"
+          log.warn("IOException while setting up watch on conf " + fileName + ". "
               + e.getMessage());
+          Thread.currentThread().interrupt();
           return;
         }
-        for (; ; ) {
-          // Watch for modifications
-          WatchKey watchKey;
-          try {
-            watchKey = watchService.take();
-          } catch (InterruptedException ie) {
-            log.warn(ie.getMessage());
-            return;
-          }
+      }
 
-          for (WatchEvent<?> event : watchKey.pollEvents()) {
-            // Make sure the modification happened to user config file
-            @SuppressWarnings("unchecked")
-            final Path name = ((WatchEvent<Path>) event).context();
-            final Path child = path.resolve(name);
-            if (!child.toString().equals(fileName)) {
-              continue; // not user xml
+      // Return if WatchService is not initialized
+      if (watchService == null) {
+        log.warn("Watchservice not setup for any config file(s).");
+        Thread.currentThread().interrupt();
+        return;
+      }
+
+      // Watchservice is established, now listen for the events till eternity!
+      for (;; ) {
+        WatchKey watchKey;
+        try {
+          watchKey = watchService.take();
+        } catch (InterruptedException ie) {
+          log.warn(ie.getMessage());
+          Thread.currentThread().interrupt();
+          return;
+        }
+
+        // Get the directory for which watch service event triggered.
+        Path dir = keys.get(watchKey);
+        for (WatchEvent<?> event : watchKey.pollEvents()) {
+          // Make sure the modification happened to user config file
+          @SuppressWarnings("unchecked")
+          final Path name = ((WatchEvent<Path>) event).context();
+          final String child = dir.resolve(name).toString();
+          // Lookup the file in dirToFilesMap
+          for (String fileName : dirToFilesMap.get(dir)) {
+            if (!child.equals(fileName)) {
+              continue;
             }
+
+            // Match!
             // reparse the config file
             log.info("Modification detected, reloading user config");
-            parser.parseConfigFile();
+            configFileMap.get(fileName).parseConfigFile();
+            break;
           }
-          watchKey.reset();
         }
+        watchKey.reset();
       }
     };
 
