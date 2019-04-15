250c00c91a9244d9ec99554607afd9a2c764e7ed
Jamie Sun <jasun@linkedin.com>
2019-04-15 13:48:34 -0700

    Fix dynamic job link url (#2189)

--- azkaban-common/src/main/java/azkaban/executor/ExecutorManager.java
+++ azkaban-common/src/main/java/azkaban/executor/ExecutorManager.java
@@ -800,7 +800,7 @@ public class ExecutorManager extends EventHandler implements
     try {
       while (!finished) {
         final LogData data = getExecutionJobLog(exFlow, jobId, offset, 50000, attempt);
-        if (data != null) {
+        if (data != null && data.getLength() != 0) {
           applicationId = findApplicationIdFromLog(data.getData());
           if (applicationId != null) {
             return applicationId;
