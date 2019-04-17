2679fa7892e223869bd86fee8c01595ed4d9c34e
Toby Allsopp <toby.allsopp@wherescape.com>
2019-04-17 17:52:38 +1200

    Update PR number in comment

--- azkaban-db/src/main/sql/upgrade.3.71.1.to.3.72.sql
+++ azkaban-db/src/main/sql/upgrade.3.71.1.to.3.72.sql
@@ -1,4 +1,4 @@
 -- DB Migration from 3.71.1 to 3.72
 
--- PR #???? changes the type of project_flows.json from BLOB to LONGBLOB
+-- PR #2192 changes the type of project_flows.json from BLOB to LONGBLOB
 ALTER TABLE project_flows ALTER COLUMN json LONGBLOB;
