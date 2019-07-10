3eec9e8cd2236e53c479637ce948208f14e588d1
juhoautio <autiojuho@gmail.com>
2019-07-10 14:31:12 +0300

    Convert if-else to ternary

--- azkaban-web-server/src/main/java/azkaban/webapp/servlet/ExecutorServlet.java
+++ azkaban-web-server/src/main/java/azkaban/webapp/servlet/ExecutorServlet.java
@@ -340,28 +340,11 @@ public class ExecutorServlet extends LoginAbstractAzkabanServlet {
     page.add("jobname", node.getId());
     page.add("jobLinkUrl", jobLinkUrl);
     page.add("jobType", node.getType());
-
-    final Status attemptStatus;
-    if (attempt == node.getAttempt()) {
-      attemptStatus = node.getStatus();
-    } else {
-      attemptStatus = node.getPastAttemptList().get(attempt).getStatus();
-    }
-    page.add("attemptStatus", attemptStatus.toString());
-
-    final int pastAttempts;
-    if (node.getAttempt() > 0) {
-      pastAttempts = node.getPastAttemptList().size();
-    } else {
-      pastAttempts = 0;
-    }
-    page.add("pastAttempts", pastAttempts);
-
-    if (node.getStatus() == Status.FAILED || node.getStatus() == Status.KILLED) {
-      page.add("jobFailed", true);
-    } else {
-      page.add("jobFailed", false);
-    }
+    page.add("attemptStatus", attempt == node.getAttempt() ?
+        node.getStatus() : node.getPastAttemptList().get(attempt).getStatus());
+    page.add("pastAttempts", node.getAttempt() > 0 ?
+        node.getPastAttemptList().size() : 0);
+    page.add("jobFailed", node.getStatus() == Status.FAILED || node.getStatus() == Status.KILLED);
 
     page.render();
   }
