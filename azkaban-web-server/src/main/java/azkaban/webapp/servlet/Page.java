82923e2af3467016c1cb094766bbb2f1ce6c9b31
Andrew Bagshaw <abagshaw@linkedin.com>
2019-11-01 16:15:17 -0700

    Escape HTML except for line breaks (#2366)
    escape html except for line breaks

--- azkaban-web-server/src/main/java/azkaban/webapp/servlet/Page.java
+++ azkaban-web-server/src/main/java/azkaban/webapp/servlet/Page.java
@@ -78,9 +78,18 @@ public class Page {
     for(Object key: this.context.getKeys()) {
       Object value = this.context.get((String)key);
       if(key instanceof String && value instanceof String) {
-        this.context.put((String)key, StringEscapeUtils.escapeHtml((String)value));
+        this.context.put((String)key, escapeHtmlExceptLineBreaks((String)value));
       }
     }
+  }
+
+  private static String escapeHtmlExceptLineBreaks(String value) {
+    // Convert line breaks to custom newline marker
+    String converted = value.replaceAll("<br/?>", "!NEWLINE!");
+    // Escape the converted string
+    String escaped = StringEscapeUtils.escapeHtml(converted);
+    // Convert newlines back to <br>
+    return escaped.replaceAll("!NEWLINE!", "<br>");
   }
 
   public void setMimeType(final String type) {
