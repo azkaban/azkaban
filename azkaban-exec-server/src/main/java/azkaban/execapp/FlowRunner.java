6d35d71be8b309540229ee4d3e0ce51c03c1e2e0
KAI XU <48968187+kxu1026@users.noreply.github.com>
2020-01-31 14:58:18 -0800

    [TASK] Adding more logs for ramp feature + fixing a bug on saving ramp status to DB (#2427)
    * [TASK] ADD MORE LOGS + FIX ONE BUG WHICH CAUSE THE CACHED STATUS CANNOT BER PERSISTANTED INTO DB
    
    * [TASK] Address Comments for adding more logs PR
    
    * [TASK] Fix the test error
    
    * Fix Test Failure Issue
    
    * Add Locker instead of volitate array

--- azkaban-exec-server/src/main/java/azkaban/execapp/FlowRunner.java
+++ azkaban-exec-server/src/main/java/azkaban/execapp/FlowRunner.java
@@ -904,7 +904,13 @@ public class FlowRunner extends EventHandler implements Runnable {
 
     node.setStatus(Status.QUEUED);
 
-    node.setRampProps(this.flow.getRampPropsForJob(node.getId(), node.getInputProps().getString("type")));
+    String jobId = node.getId();
+    String jobType = node.getInputProps().getString("type");
+    Props rampProps = this.flow.getRampPropsForJob(jobId, jobType);
+    if (rampProps != null) {
+      this.logger.info("Selected Ramp Props in Flow : [" + rampProps + "]");
+      node.setRampProps(rampProps);
+    }
 
     final JobRunner runner = createJobRunner(node);
     this.logger.info("Submitting job '" + node.getNestedId() + "' to run.");
