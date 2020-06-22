8f41f8661363a4b20deade0cfba0a961a7558294
Brandon <brandonjy@users.noreply.github.com>
2020-06-22 23:41:19 +0800

    update default log location to local folder.
    Currently, the default log location is under server parent location. So the user will see folder structure like following
    bin/
    conf/
    local/
    soloServerLog__2019-11-02+01:05:42.out
    soloServerLog__2019-11-02+01:56:07.out
    
    1. Update the default log location to local/ folder
    2. Update the getStarted doc to show how to config log configuration

--- azkaban-exec-server/src/main/bash/start-exec.sh
+++ azkaban-exec-server/src/main/bash/start-exec.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 script_dir=$(dirname $0)
+script_parent_dir=$(dirname $script_dir)
 
 # pass along command line arguments to the internal launch script.
-${script_dir}/internal/internal-start-executor.sh "$@" >executorServerLog__`date +%F+%T`.out 2>&1 &
+${script_dir}/internal/internal-start-executor.sh "$@" > ${script_parent_dir}/local/executorServerLog__`date +%F+%T`.out 2>&1 &
 
