b468bffe956d910ab1e7956b7406542e4b0c6902
Juho Autio <autiojuho@gmail.com>
2020-07-16 10:18:00 +0300

    Support sla settings in execute & schedule API actions (#2523)
    This implements "#2509 Support SLA options in execute API."
    
    As a bonus SLA options are now supported also in the schedule call because it's using the same HttpRequestUtils. This is nice because if using the API directly to add schedules (like many users do), the API now allows ensuring that a scheduled execution doesn't start running before SLA has been set for it.
    
    The separate set sla API for schedules remains as well.

--- azkaban-common/src/main/java/azkaban/sla/SlaOption.java
+++ azkaban-common/src/main/java/azkaban/sla/SlaOption.java
@@ -26,6 +26,7 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.Set;
 import java.util.stream.Collectors;
 import org.joda.time.DateTime;
@@ -341,6 +342,40 @@ public class SlaOption {
     return options.stream()
         .filter(option -> option.isComponentType(componentType))
         .collect(Collectors.toList());
+  }
+
+  @Override
+  public String toString() {
+    return "SlaOption{" +
+        "type=" + type +
+        ", flowName='" + flowName + '\'' +
+        ", jobName='" + jobName + '\'' +
+        ", duration=" + duration +
+        ", actions=" + actions +
+        ", emails=" + emails +
+        '}';
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    SlaOption slaOption = (SlaOption) o;
+    return type == slaOption.type &&
+        Objects.equals(flowName, slaOption.flowName) &&
+        Objects.equals(jobName, slaOption.jobName) &&
+        Objects.equals(duration, slaOption.duration) &&
+        Objects.equals(actions, slaOption.actions) &&
+        Objects.equals(emails, slaOption.emails);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(type, flowName, jobName, duration, actions, emails);
   }
 
   /**
