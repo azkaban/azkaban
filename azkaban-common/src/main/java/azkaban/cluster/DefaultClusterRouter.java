9d18569e1a7899b38fee3c6db8d4e9ae02a3cc71
Haibo Chen <hachen@linkedin.com>
2020-07-27 08:52:54 -0700

    BDP-2280. Expose cluster routing exceptions to job logger for easy debugging.
    RB=2204342
    G=azkabandev-reviewers
    R=djaiswal,jakhani
    A=jakhani

--- azkaban-common/src/main/java/azkaban/cluster/DefaultClusterRouter.java
+++ azkaban-common/src/main/java/azkaban/cluster/DefaultClusterRouter.java
@@ -5,6 +5,7 @@ import org.apache.hadoop.conf.Configuration;
 
 import javax.inject.Inject;
 import java.util.Collection;
+import org.apache.log4j.Logger;
 
 /**
  * An implementation of {@link ClusterRouter} that always routes jobs to the default cluster.
@@ -16,9 +17,12 @@ public class DefaultClusterRouter extends ClusterRouter {
   }
 
   @Override
-  public Cluster getCluster(final String jobId, final Props jobProps, final Collection<String> componentDependency) {
+  public Cluster getCluster(final String jobId, final Props jobProps,
+      final Logger jobLogger, final Collection<String> componentDependency) {
     final Cluster cluster = this.clusterRegistry.getCluster(Cluster.DEFAULT_CLUSTER);
     if (cluster == null) {
+      jobLogger.error(
+          String.format("Could not load cluster: %s for job %s", Cluster.DEFAULT_CLUSTER, jobId));
       throw new RuntimeException("The default cluster is not found");
     }
     return cluster;
