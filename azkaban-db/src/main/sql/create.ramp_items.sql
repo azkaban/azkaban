ac447606f307fc61abef2d5c67e5a87317f2f9e2
Yeni Bermudez Padron <ypadron@linkedin.com>
2020-11-06 13:33:48 -0800

    Expand the length of some fields related to Ramp Feature (#2630)
    * Expand the length of some fields related to Ramp Featur
    
    RB=2359532
    BUG=LIHADOOP-56459
    G=azkabandev-reviewers
    R=adsharma,abtiwari,ygerchik,jakhani,sarumuga,angoel,soudas,areznik,djaiswal,sshardoo,gsalia,apruthi,anismalh,prkhande,anath1,ypadron
    A=gsalia

--- azkaban-db/src/main/sql/create.ramp_items.sql
+++ azkaban-db/src/main/sql/create.ramp_items.sql
@@ -1,7 +1,7 @@
 CREATE TABLE ramp_items (
   rampId VARCHAR(45) NOT NULL,
-  dependency VARCHAR(45) NOT NULL,
-  rampValue VARCHAR (500) NOT NULL,
+  dependency VARCHAR(200) NOT NULL,
+  rampValue VARCHAR (1000) NOT NULL,
   PRIMARY KEY (rampId, dependency)
 );
 
