44e78c8953d419e78777a2c5f445517008e911cb
mactaggart <mactaggart@users.noreply.github.com>
2021-01-24 11:17:49 -0800

    remove shutdown of cleanerThread as it was removed

--- azkaban-common/src/main/java/azkaban/executor/ExecutorManager.java
+++ azkaban-common/src/main/java/azkaban/executor/ExecutorManager.java
@@ -660,9 +660,6 @@ public class ExecutorManager extends AbstractExecutorManagerAdapter {
     if (null != this.updaterThread) {
       this.updaterThread.shutdown();
     }
-    if(null != this.cleanerThread){
-      this.cleanerThread.shutdown();
-    }
   }
 
   /**
