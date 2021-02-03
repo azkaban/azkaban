azkaban/azkaban

commit 38f3c103b771d1cf4687f9e97a0e0537dea1a1e4

Add executor api calls for containerized flows
Modifies ExecutorApiClient and ExecutorApiGateway for
 - Handling Reverse-proxy based access to Executors/FlowContainers
 - Containerized dispatch to FlowContainers

Add executor api call invocations from ContainerizedDispatchManager
 - Done indirectly by adding invocation in base class AbstractDispatchManager
 - Adds getExecutableFlowLog, getExecutionJobLog, getExecutionJobStats, cancelFlow

azkaban-common/src/main/java/azkaban/executor/ExecutorApiGateway.java
@@ -16,8 +16,13 @@
 
 package azkaban.executor;
 
+import azkaban.Constants;
+import azkaban.Constants.ConfigurationKeys;
+import azkaban.DispatchMethod;
 import azkaban.utils.JSONUtils;
 import azkaban.utils.Pair;
+import azkaban.utils.Props;
+import com.google.common.annotations.VisibleForTesting;
 import com.google.inject.Inject;
 import java.io.IOException;
 import java.net.URI;
@@ -25,17 +30,43 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
+import java.util.function.BiFunction;
 import javax.inject.Singleton;
 import org.codehaus.jackson.map.ObjectMapper;
 
 @Singleton
 public class ExecutorApiGateway {
+  public final static String DEFAULT_EXECUTION_RESOURCE = "executor";
+  public final static String CONTAINERIZED_EXECUTION_RESOURCE = "container";
 
+  // Default procedure for modifying a resource path that a reverse proxy, such as an
+  // ingress-controller, can use to route the request to correct endpoint.
+  //   - This is a first-class function to make it easier to switch to a different mechanism of
+  //     creating the path, depending on how the reverse-proxy is configured.
+  //   - In future this implementation could be guice-injected (possibly based on a config property)
+  //   - This implementation simply prefixes resource name with the execution-id and assumes that
+  //     that a reverse proxy can route the request correctly based on this prefix.
+  private final static BiFunction<Integer, String, String> executionResourceNameModifier =
+      ((e,r) -> String.join("/",  e.toString(), r));
+
+  private final static Executor defaultEmptyExecutor = new Executor(-1, "", 1, false);
   private final ExecutorApiClient apiClient;
+  private final String executionResourceName;
+  private final boolean isReverseProxyEnabled;
 
   @Inject
-  public ExecutorApiGateway(final ExecutorApiClient apiClient) {
+  public ExecutorApiGateway(final ExecutorApiClient apiClient, Props azkProps) {
     this.apiClient = apiClient;
+    isReverseProxyEnabled =
+        azkProps.getBoolean(ConfigurationKeys.AZKABAN_EXECUTOR_REVERSE_PROXY_ENABLED,
+            false);
+    String executionResourceName = DEFAULT_EXECUTION_RESOURCE;
+    if (DispatchMethod.getDispatchMethod(azkProps
+        .getString(Constants.ConfigurationKeys.AZKABAN_EXECUTION_DISPATCH_METHOD,
+            DispatchMethod.PUSH.name())) == DispatchMethod.CONTAINERIZED) {
+      executionResourceName = CONTAINERIZED_EXECUTION_RESOURCE;
+    }
+    this.executionResourceName = executionResourceName;
   }
 
   Map<String, Object> callWithExecutable(final ExecutableFlow exflow,
@@ -46,7 +77,7 @@ public class ExecutorApiGateway {
 
   Map<String, Object> callWithReference(final ExecutionReference ref, final String action,
       final Pair<String, String>... params) throws ExecutorManagerException {
-    final Executor executor = ref.getExecutor().get();
+    final Executor executor = (isReverseProxyEnabled ? defaultEmptyExecutor : ref.getExecutor().get());
     return callWithExecutionId(executor.getHost(), executor.getPort(), action, ref.getExecId(),
         null, params);
   }
@@ -54,9 +85,17 @@ public class ExecutorApiGateway {
   Map<String, Object> callWithReferenceByUser(final ExecutionReference ref,
       final String action, final String user, final Pair<String, String>... params)
       throws ExecutorManagerException {
-    final Executor executor = ref.getExecutor().get();
+    final Executor executor = (isReverseProxyEnabled ? defaultEmptyExecutor : ref.getExecutor().get());
     return callWithExecutionId(executor.getHost(), executor.getPort(), action,
         ref.getExecId(), user, params);
+  }
+
+  @VisibleForTesting
+  String createExecutionPath(int execId) {
+    if (!isReverseProxyEnabled) {
+      return "/" + executionResourceName;
+    }
+    return "/" + executionResourceNameModifier.apply(execId, executionResourceName);
   }
 
   Map<String, Object> callWithExecutionId(final String host, final int port,
@@ -75,7 +114,10 @@ public class ExecutorApiGateway {
           .valueOf(executionId)));
       paramList.add(new Pair<>(ConnectorParams.USER_PARAM, user));
 
-      return callForJsonObjectMap(host, port, "/executor", paramList);
+      // Ideally we should throw an exception if executionId is null but some existing code
+      // (updateExecutions()) expects to call this method with a null executionId.
+      String executionPath = (executionId == null) ? null : createExecutionPath(executionId);
+      return callForJsonObjectMap(host, port, executionPath, paramList);
     } catch (final IOException e) {
       throw new ExecutorManagerException(e.getMessage(), e);
     }
@@ -120,7 +162,7 @@ public class ExecutorApiGateway {
     }
 
     @SuppressWarnings("unchecked") final URI uri =
-        ExecutorApiClient.buildUri(host, port, path, true);
+        apiClient.buildExecutorUri(host, port, path, true);
 
     return this.apiClient.httpPost(uri, paramList);
   }

azkaban-common/src/test/java/azkaban/executor/ExecutorApiGatewayTest.java
@@ -18,11 +18,17 @@ package azkaban.executor;
 
 import static org.junit.Assert.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyBoolean;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.when;
 
+import azkaban.Constants.ConfigurationKeys;
+import azkaban.DispatchMethod;
 import azkaban.utils.JSONUtils;
 import azkaban.utils.Pair;
+import azkaban.utils.Props;
 import com.google.common.collect.ImmutableMap;
 import java.net.URI;
 import java.util.Collections;
@@ -47,7 +53,7 @@ public class ExecutorApiGatewayTest {
   public void setUp() throws Exception {
     MockitoAnnotations.initMocks(this);
     this.client = Mockito.mock(ExecutorApiClient.class);
-    this.gateway = new ExecutorApiGateway(this.client);
+    this.gateway = new ExecutorApiGateway(this.client, new Props());
   }
 
   @Test
@@ -65,7 +71,7 @@ public class ExecutorApiGatewayTest {
   public void updateExecutions() throws Exception {
     final ImmutableMap<String, String> map = ImmutableMap.of("test", "response");
     when(this.client
-        .httpPost(eq(new URI("http://executor-2:1234/executor")), this.params.capture()))
+        .httpPost(any(), this.params.capture()))
         .thenReturn(JSONUtils.toJSON(map));
     final Map<String, Object> response = this.gateway
         .updateExecutions(new Executor(2, "executor-2", 1234, true),
@@ -76,5 +82,26 @@ public class ExecutorApiGatewayTest {
     assertEquals(new Pair<>("action", "update"), this.params.getValue().get(2));
     assertEquals(new Pair<>("execid", "null"), this.params.getValue().get(3));
     assertEquals(new Pair<>("user", null), this.params.getValue().get(4));
+  }
+
+  @Test
+  public void testPathWithDefaultConfigs() {
+    int executionId = 12345;
+    String path = gateway.createExecutionPath(12345);
+    Assert.assertEquals("/"+ExecutorApiGateway.DEFAULT_EXECUTION_RESOURCE, path);
+  }
+
+  @Test
+  public void testPathWithContainerizedConfigs() {
+    Props containerizedConfigs = new Props();
+    containerizedConfigs.put(ConfigurationKeys.AZKABAN_EXECUTOR_REVERSE_PROXY_ENABLED, "true");
+    containerizedConfigs.put(ConfigurationKeys.AZKABAN_EXECUTION_DISPATCH_METHOD,
+        DispatchMethod.CONTAINERIZED.name());
+    ExecutorApiGateway gateway = new ExecutorApiGateway(client, containerizedConfigs);
+    int executionId = 12345;
+    String path = gateway.createExecutionPath(12345);
+    Assert.assertEquals("/" + executionId + "/" +
+            ExecutorApiGateway.CONTAINERIZED_EXECUTION_RESOURCE,
+        path);
   }
 }

az-core/src/main/java/azkaban/Constants.java
@@ -400,6 +400,13 @@ public class Constants {
     //    oauth.redirect_uri=http://localhost:8081/?action=oauth_callback
 
     public static final String EXECUTOR_CONNECTION_TLS_ENABLED = "executor.connection.tls.enabled";
+
+    public static final String AZKABAN_EXECUTOR_REVERSE_PROXY_ENABLED =
+        "azkaban.executor.reverse.proxy.enabled";
+    public static final String AZKABAN_EXECUTOR_REVERSE_PROXY_HOSTNAME =
+        "azkaban.executor.reverse.proxy.hostname";
+    public static final String AZKABAN_EXECUTOR_REVERSE_PROXY_PORT =
+        "azkaban.executor.reverse.proxy.port";
   }
 
   public static class FlowProperties {

azkaban-common/src/main/java/azkaban/executor/AbstractExecutorManagerAdapter.java
@@ -17,6 +17,7 @@ package azkaban.executor;
 
 import azkaban.Constants;
 import azkaban.Constants.ConfigurationKeys;
+import azkaban.DispatchMethod;
 import azkaban.event.EventHandler;
 import azkaban.flow.FlowUtils;
 import azkaban.metrics.CommonMetrics;
@@ -54,6 +55,7 @@ public abstract class AbstractExecutorManagerAdapter extends EventHandler implem
   protected final ExecutorLoader executorLoader;
   protected final CommonMetrics commonMetrics;
   protected final ExecutorApiGateway apiGateway;
+  private final AlerterHolder alerterHolder;
   private final int maxConcurrentRunsOneFlow;
   private final Map<Pair<String, String>, Integer> maxConcurrentRunsPerFlowMap;
   private static final Duration RECENTLY_FINISHED_LIFETIME = Duration.ofMinutes(10);
@@ -61,11 +63,13 @@ public abstract class AbstractExecutorManagerAdapter extends EventHandler implem
   protected AbstractExecutorManagerAdapter(final Props azkProps,
       final ExecutorLoader executorLoader,
       final CommonMetrics commonMetrics,
-      final ExecutorApiGateway apiGateway) {
+      final ExecutorApiGateway apiGateway,
+      final AlerterHolder alerterHolder) {
     this.azkProps = azkProps;
     this.executorLoader = executorLoader;
     this.commonMetrics = commonMetrics;
     this.apiGateway = apiGateway;
+    this.alerterHolder = alerterHolder;
     this.maxConcurrentRunsOneFlow = ExecutorUtils.getMaxConcurrentRunsOneFlow(azkProps);
     this.maxConcurrentRunsPerFlowMap = ExecutorUtils.getMaxConcurentRunsPerFlowMap(azkProps);
   }
@@ -269,7 +273,8 @@ public abstract class AbstractExecutorManagerAdapter extends EventHandler implem
     }
   }
 
-  protected String uploadExecutableFlow(ExecutableFlow exflow, String userId, String flowId,
+  protected String uploadExecutableFlow(
+      final ExecutableFlow exflow, final String userId, final String flowId,
       String message) throws ExecutorManagerException {
     final int projectId = exflow.getProjectId();
     exflow.setSubmitUser(userId);
@@ -442,6 +447,60 @@ public abstract class AbstractExecutorManagerAdapter extends EventHandler implem
         .get("attachments");
 
     return jobStats;
+  }
+
+  @Override
+  public LogData getExecutableFlowLog(final ExecutableFlow exFlow, final int offset,
+      final int length) throws ExecutorManagerException {
+    final Pair<ExecutionReference, ExecutableFlow> pair = this.executorLoader
+        .fetchActiveFlowByExecId(exFlow.getExecutionId());
+    return getFlowLogData(exFlow, offset, length, pair);
+  }
+
+  @Override
+  public LogData getExecutionJobLog(final ExecutableFlow exFlow, final String jobId,
+      final int offset, final int length, final int attempt) throws ExecutorManagerException {
+    final Pair<ExecutionReference, ExecutableFlow> pair = this.executorLoader
+        .fetchActiveFlowByExecId(exFlow.getExecutionId());
+    return getJobLogData(exFlow, jobId, offset, length, attempt, pair);
+  }
+
+  @Override
+  public List<Object> getExecutionJobStats(final ExecutableFlow exFlow, final String jobId,
+      final int attempt) throws ExecutorManagerException {
+    final Pair<ExecutionReference, ExecutableFlow> pair =
+        this.executorLoader.fetchActiveFlowByExecId(exFlow.getExecutionId());
+    return getExecutionJobStats(exFlow, jobId, attempt, pair);
+  }
+
+  /**
+   * If a flow is already dispatched to an executor, cancel by calling Executor. Else if it's still
+   * queued in DB, remove it from DB queue and finalize. {@inheritDoc}
+   */
+  @Override
+  public void cancelFlow(final ExecutableFlow exFlow, final String userId)
+      throws ExecutorManagerException {
+    synchronized (exFlow) {
+      final Map<Integer, Pair<ExecutionReference, ExecutableFlow>> unfinishedFlows = this.executorLoader
+          .fetchUnfinishedFlows();
+      if (unfinishedFlows.containsKey(exFlow.getExecutionId())) {
+        final Pair<ExecutionReference, ExecutableFlow> pair = unfinishedFlows
+            .get(exFlow.getExecutionId());
+        if (pair.getFirst().getExecutor().isPresent()) {
+          // Flow is already dispatched to an executor, so call that executor to cancel the flow.
+          this.apiGateway
+              .callWithReferenceByUser(pair.getFirst(), ConnectorParams.CANCEL_ACTION, userId);
+        } else {
+          // Flow is still queued, need to finalize it and update the status in DB.
+          ExecutionControllerUtils.finalizeFlow(this.executorLoader, this.alerterHolder, exFlow,
+              "Cancelled before dispatching to executor", null);
+        }
+      } else {
+        throw new ExecutorManagerException("Execution "
+            + exFlow.getExecutionId() + " of flow " + exFlow.getFlowId()
+            + " isn't running.");
+      }
+    }
   }
 
   protected Map<String, Object> modifyExecutingJobs(final ExecutableFlow exFlow,

azkaban-common/src/main/java/azkaban/executor/ExecutionController.java
@@ -41,7 +41,6 @@ import org.slf4j.LoggerFactory;
 public class ExecutionController extends AbstractExecutorManagerAdapter {
 
   private static final Logger logger = LoggerFactory.getLogger(ExecutionController.class);
-  private final AlerterHolder alerterHolder;
   private final ExecutorHealthChecker executorHealthChecker;
 
 
@@ -50,8 +49,7 @@ public class ExecutionController extends AbstractExecutorManagerAdapter {
       final CommonMetrics commonMetrics,
       final ExecutorApiGateway apiGateway, final AlerterHolder alerterHolder, final
   ExecutorHealthChecker executorHealthChecker) {
-    super(azkProps, executorLoader, commonMetrics, apiGateway);
-    this.alerterHolder = alerterHolder;
+    super(azkProps, executorLoader, commonMetrics, apiGateway, alerterHolder);
     this.executorHealthChecker = executorHealthChecker;
   }
 
@@ -182,60 +180,6 @@ public class ExecutionController extends AbstractExecutorManagerAdapter {
   @Override
   public DispatchMethod getDispatchMethod() {
     return DispatchMethod.POLL;
-  }
-
-  @Override
-  public LogData getExecutableFlowLog(final ExecutableFlow exFlow, final int offset,
-      final int length) throws ExecutorManagerException {
-    final Pair<ExecutionReference, ExecutableFlow> pair = this.executorLoader
-        .fetchActiveFlowByExecId(exFlow.getExecutionId());
-    return getFlowLogData(exFlow, offset, length, pair);
-  }
-
-  @Override
-  public LogData getExecutionJobLog(final ExecutableFlow exFlow, final String jobId,
-      final int offset, final int length, final int attempt) throws ExecutorManagerException {
-    final Pair<ExecutionReference, ExecutableFlow> pair = this.executorLoader
-        .fetchActiveFlowByExecId(exFlow.getExecutionId());
-    return getJobLogData(exFlow, jobId, offset, length, attempt, pair);
-  }
-
-  @Override
-  public List<Object> getExecutionJobStats(final ExecutableFlow exFlow, final String jobId,
-      final int attempt) throws ExecutorManagerException {
-    final Pair<ExecutionReference, ExecutableFlow> pair =
-        this.executorLoader.fetchActiveFlowByExecId(exFlow.getExecutionId());
-    return getExecutionJobStats(exFlow, jobId, attempt, pair);
-  }
-
-  /**
-   * If a flow is already dispatched to an executor, cancel by calling Executor. Else if it's still
-   * queued in DB, remove it from DB queue and finalize. {@inheritDoc}
-   */
-  @Override
-  public void cancelFlow(final ExecutableFlow exFlow, final String userId)
-      throws ExecutorManagerException {
-    synchronized (exFlow) {
-      final Map<Integer, Pair<ExecutionReference, ExecutableFlow>> unfinishedFlows = this.executorLoader
-          .fetchUnfinishedFlows();
-      if (unfinishedFlows.containsKey(exFlow.getExecutionId())) {
-        final Pair<ExecutionReference, ExecutableFlow> pair = unfinishedFlows
-            .get(exFlow.getExecutionId());
-        if (pair.getFirst().getExecutor().isPresent()) {
-          // Flow is already dispatched to an executor, so call that executor to cancel the flow.
-          this.apiGateway
-              .callWithReferenceByUser(pair.getFirst(), ConnectorParams.CANCEL_ACTION, userId);
-        } else {
-          // Flow is still queued, need to finalize it and update the status in DB.
-          ExecutionControllerUtils.finalizeFlow(this.executorLoader, this.alerterHolder, exFlow,
-              "Cancelled before dispatching to executor", null);
-        }
-      } else {
-        throw new ExecutorManagerException("Execution "
-            + exFlow.getExecutionId() + " of flow " + exFlow.getFlowId()
-            + " isn't running.");
-      }
-    }
   }
 
   @Override

commit a9a8b8f07cc2bfea8457a02ddcf84ee92a78b8ec

Fix handling of null executionId in ExecutorApiGateway (#2752)
Handling of null executionId in ExecutorApiGateway.callWithExecutionId() was broken by commit: 
https://github.com/azkaban/azkaban/pull/2715/commits/38f3c103b771d1cf4687f9e97a0e0537dea1a1e4

azkaban-common/src/main/java/azkaban/executor/ExecutorApiGateway.java
@@ -30,12 +30,16 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.function.BiFunction;
 import javax.inject.Singleton;
 import org.codehaus.jackson.map.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Singleton
 public class ExecutorApiGateway {
+  private final static Logger logger = LoggerFactory.getLogger(ExecutorApiGateway.class);
   public final static String DEFAULT_EXECUTION_RESOURCE = "executor";
   public final static String CONTAINERIZED_EXECUTION_RESOURCE = "container";
 
@@ -91,11 +95,17 @@ public class ExecutorApiGateway {
   }
 
   @VisibleForTesting
-  String createExecutionPath(int execId) {
+  String createExecutionPath(final Optional<Integer> executionId) throws ExecutorManagerException {
     if (!isReverseProxyEnabled) {
       return "/" + executionResourceName;
     }
-    return "/" + executionResourceNameModifier.apply(execId, executionResourceName);
+
+    if(!executionId.isPresent()) {
+      final String errorMessage = "Execution Id must be provided when reverse-proxy is enabled";
+      logger.error(errorMessage);
+      throw new ExecutorManagerException(errorMessage);
+    }
+    return "/" + executionResourceNameModifier.apply(executionId.get(), executionResourceName);
   }
 
   Map<String, Object> callWithExecutionId(final String host, final int port,
@@ -116,7 +126,7 @@ public class ExecutorApiGateway {
 
       // Ideally we should throw an exception if executionId is null but some existing code
       // (updateExecutions()) expects to call this method with a null executionId.
-      String executionPath = (executionId == null) ? null : createExecutionPath(executionId);
+      String executionPath = createExecutionPath(Optional.ofNullable(executionId));
       return callForJsonObjectMap(host, port, executionPath, paramList);
     } catch (final IOException e) {
       throw new ExecutorManagerException(e.getMessage(), e);

azkaban-common/src/test/java/azkaban/executor/ExecutorApiGatewayTest.java
@@ -16,11 +16,12 @@
 
 package azkaban.executor;
 
+import static azkaban.Constants.ConfigurationKeys.AZKABAN_EXECUTOR_REVERSE_PROXY_HOSTNAME;
+import static azkaban.Constants.ConfigurationKeys.AZKABAN_EXECUTOR_REVERSE_PROXY_PORT;
+import static azkaban.executor.ExecutorApiClientTest.REVERSE_PROXY_HOST;
+import static azkaban.executor.ExecutorApiClientTest.REVERSE_PROXY_PORT;
 import static org.junit.Assert.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyBoolean;
-import static org.mockito.ArgumentMatchers.anyInt;
-import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.when;
 
@@ -29,11 +30,15 @@ import azkaban.DispatchMethod;
 import azkaban.utils.JSONUtils;
 import azkaban.utils.Pair;
 import azkaban.utils.Props;
+import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
+import java.io.IOException;
 import java.net.URI;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
+import org.apache.http.client.methods.HttpUriRequest;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -85,23 +90,101 @@ public class ExecutorApiGatewayTest {
   }
 
   @Test
-  public void testPathWithDefaultConfigs() {
-    int executionId = 12345;
-    String path = gateway.createExecutionPath(12345);
+  public void testPathWithDefaultConfigs() throws Exception {
+    final int executionId = 12345;
+    final String path = gateway.createExecutionPath(Optional.of(executionId));
     Assert.assertEquals("/"+ExecutorApiGateway.DEFAULT_EXECUTION_RESOURCE, path);
   }
 
   @Test
-  public void testPathWithContainerizedConfigs() {
-    Props containerizedConfigs = new Props();
-    containerizedConfigs.put(ConfigurationKeys.AZKABAN_EXECUTOR_REVERSE_PROXY_ENABLED, "true");
-    containerizedConfigs.put(ConfigurationKeys.AZKABAN_EXECUTION_DISPATCH_METHOD,
-        DispatchMethod.CONTAINERIZED.name());
-    ExecutorApiGateway gateway = new ExecutorApiGateway(client, containerizedConfigs);
-    int executionId = 12345;
-    String path = gateway.createExecutionPath(12345);
+  public void testPathWithContainerizedConfigs() throws Exception {
+    final ExecutorApiGateway gateway = gatewayWithConfigs(this.client, containerizationEnabledProps());
+    final int executionId = 12345;
+    final String path = gateway.createExecutionPath(Optional.of(executionId));
     Assert.assertEquals("/" + executionId + "/" +
             ExecutorApiGateway.CONTAINERIZED_EXECUTION_RESOURCE,
         path);
+  }
+
+  private Props containerizationEnabledProps() {
+    final Props containerizedProps = new Props();
+    containerizedProps.put(ConfigurationKeys.AZKABAN_EXECUTOR_REVERSE_PROXY_ENABLED, "true");
+    containerizedProps.put(AZKABAN_EXECUTOR_REVERSE_PROXY_HOSTNAME, REVERSE_PROXY_HOST);
+    containerizedProps.put(AZKABAN_EXECUTOR_REVERSE_PROXY_PORT, REVERSE_PROXY_PORT);
+    containerizedProps.put(ConfigurationKeys.AZKABAN_EXECUTION_DISPATCH_METHOD,
+        DispatchMethod.CONTAINERIZED.name());
+    return containerizedProps;
+  }
+
+  private ExecutorApiGateway gatewayWithConfigs(final ExecutorApiClient client, final Props props) {
+    return new ExecutorApiGateway(client, props);
+  }
+
+  @Test
+  public void testPingWithDefaultConfigs() throws Exception {
+    final Props props = new Props();
+    final ExecutorApiClient clientSpy = Mockito.spy(new SendDisabledExecutorApiClient(props));
+
+    final ExecutorApiGateway gateway = gatewayWithConfigs(clientSpy, props);
+    final String apiAction = ConnectorParams.PING_ACTION;
+    final String apiHost = "host1";
+    final int apiPort = 1234;
+    final Map<String, Object> response = gateway.callWithExecutionId(apiHost, apiPort, apiAction,
+        null, null);
+
+    final URI expectedUri = new URI("http://host1:1234/executor");
+    final List<Pair<String, String>> expectedParams = ImmutableList.of(
+        new Pair<>("action", "ping"),
+        new Pair<>("execid", "null"), // this is "null" (string) due to application of String.valueOf()
+        new Pair<>("user", null));
+    Mockito.verify(clientSpy).httpPost(eq(expectedUri), eq(expectedParams));
+  }
+
+  @Test
+  public void testPingWithContainerization() throws Exception {
+    final Props props = new Props();
+    props.put(ConfigurationKeys.AZKABAN_EXECUTION_DISPATCH_METHOD, DispatchMethod.CONTAINERIZED.name());
+    final ExecutorApiClient clientSpy = Mockito.spy(new SendDisabledExecutorApiClient(props));
+    final ExecutorApiGateway gateway = gatewayWithConfigs(clientSpy, props);
+    final String apiAction = ConnectorParams.PING_ACTION;
+    final String apiHost = "host1";
+    final int apiPort = 1234;
+    final Map<String, Object> response = gateway.callWithExecutionId(apiHost, apiPort, apiAction,
+        null, null);
+
+    final URI expectedUri = new URI("http://host1:1234/container");
+    final List<Pair<String, String>> expectedParams = ImmutableList.of(
+        new Pair<>("action", "ping"),
+        new Pair<>("execid", "null"),
+        new Pair<>("user", null));
+    Mockito.verify(clientSpy).httpPost(eq(expectedUri), eq(expectedParams));
+  }
+
+  @Test(expected = ExecutorManagerException.class)
+  public void testPingWithReverseProxy() throws Exception {
+    final Props props = containerizationEnabledProps();
+    final ExecutorApiClient client = new SendDisabledExecutorApiClient(props);
+    final ExecutorApiGateway gateway = gatewayWithConfigs(client, props);
+    final String apiAction = ConnectorParams.PING_ACTION;
+    final String apiHost = "host1";
+    final int apiPort = 1234;
+
+    // this should throw an exception as the properties have reverse-proxy enabled.
+    final Map<String, Object> response = gateway.callWithExecutionId(apiHost, apiPort, apiAction,
+        null, null);
+    Assert.fail();
+  }
+
+  private static class SendDisabledExecutorApiClient extends ExecutorApiClient {
+    private static String SUCCESS_JSON = "{\"status\":\"success\"}";
+
+    public SendDisabledExecutorApiClient(Props azkProps) {
+      super(azkProps);
+    }
+
+    @Override
+    protected String sendAndReturn(HttpUriRequest request) throws IOException {
+      return SUCCESS_JSON;
+    }
   }
 }