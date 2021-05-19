674237d28fe3a7efe810997172bc3c995db8e6fb
Janki Akhani <jakhani@linkedin.com>
2021-05-19 16:38:38 -0700

    Marking nscd mount as read only

--- az-core/src/main/java/azkaban/Constants.java
+++ az-core/src/main/java/azkaban/Constants.java
@@ -599,11 +599,11 @@ public class Constants {
     public static final String KUBERNETES_POD_PREFIX = AZKABAN_KUBERNETES_PREFIX + "pod.";
     public static final String KUBERNETES_POD_NAME_PREFIX = KUBERNETES_POD_PREFIX + "name.prefix";
     public static final String KUBERNETES_POD_NSCD_SOCKET_VOLUME_MOUNT_PATH =
-        AZKABAN_KUBERNETES_PREFIX + "nscd.socket.volume.mount.path";
+        KUBERNETES_POD_PREFIX + "nscd.socket.volume.mount.path";
     public static final String KUBERNETES_POD_NSCD_SOCKET_HOST_PATH =
-        AZKABAN_KUBERNETES_PREFIX + "nscd.socket.host.path";
+        KUBERNETES_POD_PREFIX + "nscd.socket.host.path";
     public static final String KUBERNETES_POD_NSCD_MOUNT_READ_ONLY =
-        AZKABAN_KUBERNETES_PREFIX + "nscd.mount.read.only";
+        KUBERNETES_POD_PREFIX + "nscd.mount.read.only";
 
     // Kubernetes flow container related properties
     public static final String KUBERNETES_FLOW_CONTAINER_PREFIX = AZKABAN_KUBERNETES_PREFIX +
