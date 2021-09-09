ab0c5f1cc170d375b4cd3144bb8df523e7d04510
juhoautio <autiojuho@gmail.com>
2021-09-10 00:53:13 +0300

    Set versions for db upgrade script

--- azkaban-db/src/main/sql/updgrade.2ab81c191047f14a77adbfe8eda3bcaa56974cfd.sql
+++
@@ -1,9 +0,0 @@
--- TODO what are the from & to versions for this?
--- the original change of create.execution_flows.sql was done in commit:
--- 2ab81c191047f14a77adbfe8eda3bcaa56974cfd
-
--- DB Migration from release ??? to ???
--- Adding dispatch_method column in execution_flows
-ALTER TABLE execution_flows ADD COLUMN dispatch_method TINYINT DEFAULT 1;
-
-CREATE INDEX ex_flows_dispatch_method ON execution_flows (dispatch_method);
