e0d6e1684b7209f24b98368413e0a968b0ceb26e
mactaggart <mactaggart@user.noreply.github.com>
2021-12-22 14:33:42 -0800

    bump license year

--- azkaban-web-server/src/test/java/azkaban/client/AzkabanClient.java
+++ azkaban-web-server/src/test/java/azkaban/client/AzkabanClient.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2020 LinkedIn Corp.
+ * Copyright 2021 LinkedIn Corp.
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
@@ -12,7 +12,6 @@
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
  * License for the specific language governing permissions and limitations under
  * the License.
- *
  */
 package azkaban.client;
 
