fee4f227bdef801cdf9c1e40c3b4a7b83b4afb34
Adam Mielke <amielke@linkedin.com>
2022-01-11 09:21:50 -0800

    Changes to logging story

--- azkaban-common/src/main/java/azkaban/executor/container/watch/FlowStatusManagerListener.java
+++ azkaban-common/src/main/java/azkaban/executor/container/watch/FlowStatusManagerListener.java
@@ -325,26 +325,10 @@ public class FlowStatusManagerListener extends EventHandler implements AzPodStat
           event.getPodName(), originalFlowStatus.get()));
       logPodDetails(event);
     }
+    if (originalFlowStatus.isPresent() && Status.isStatusToLog(originalFlowStatus.get())) {
+      logPodDetails(event);
+    }
     deleteFlowContainer(event);
-  }
-
-  private Optional<Status> getOriginalStatus(AzPodStatusMetadata event) {
-    int executionId = Integer.parseInt(event.getFlowPodMetadata().get().getExecutionId());
-    ExecutableFlow executableFlow = null;
-    try {
-      executableFlow = executorLoader.fetchExecutableFlow(executionId);
-    } catch (ExecutorManagerException e) {
-      String message = format("Exception while fetching executable flow for pod %s",
-          event.getPodName());
-      logger.error(message, e);
-      throw new AzkabanWatchException(message, e);
-    }
-    if (executableFlow == null) {
-      logger.error("Unable to find executable flow for execution: " + executionId);
-      return Optional.empty();
-    }
-    Status originalStatus = executableFlow.getStatus();
-    return Optional.of(originalStatus);
   }
 
   private void logPodDetails(AzPodStatusMetadata event) {
