9300736efc9f8913db647b64d1dd544b6ce5b96c
Binyao Jiang <bijiang@linkedin.com>
2022-07-21 10:06:19 -0700

    Add flowEndTime to fetchLogs interface

--- azkaban-common/src/main/java/azkaban/logs/ExecutionLogsLoader.java
+++ azkaban-common/src/main/java/azkaban/logs/ExecutionLogsLoader.java
@@ -9,8 +9,11 @@ public interface ExecutionLogsLoader {
   void uploadLogFile(int execId, String name, int attempt, File... files)
       throws ExecutorManagerException;
 
+  // FlowEndTime is needed for offline logs to tell if the logs are complete or not considering
+  // every offline logging platform has decent amount of delay from when log-is-sent to when
+  // log-is-available.
   LogData fetchLogs(int execId, String name, int attempt, int startByte,
-      int length) throws ExecutorManagerException;
+      int length, long flowEndTime) throws ExecutorManagerException;
 
   int removeExecutionLogsByTime(long millis, int recordCleanupLimit)
       throws ExecutorManagerException;
