bd68f12c0972c26078eafc6256d020ba75f1ef4f
Binyao Jiang <bijiang@linkedin.com>
2022-08-12 09:31:06 -0700

    Remove Ignore tag from flowShouldFailWhenConditionalParameterDoesntExist (#3137)
    Co-authored-by: Yujing Xie <yujxie@yujxie-mn1.linkedin.biz>
    
    PR link: https://github.com/azkaban/azkaban/pull/3137
    
    RB=3552409
    G=azkabandev-reviewers
    R=sarumuga,abtiwari,sshardoo,adsharma,areznik,gsalia,djaiswal,jakhani,prkhande,ypadron,anath1,apruthi,angoel
    A=amielke

--- azkaban-exec-server/src/test/java/azkaban/execapp/FlowRunnerConditionalFlowTest.java
+++ azkaban-exec-server/src/test/java/azkaban/execapp/FlowRunnerConditionalFlowTest.java
@@ -100,7 +100,6 @@ public class FlowRunnerConditionalFlowTest extends FlowRunnerTestBase {
   }
 
   @Test
-  @Ignore
   public void flowShouldFailWhenConditionalParameterDoesntExist() throws Exception {
     final HashMap<String, String> flowProps = new HashMap<>();
     setUp(CONDITIONAL_FLOW_8, flowProps);
