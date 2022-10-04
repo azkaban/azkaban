94b487c42ee1a145a400014e4376bbe9a9b7d7d5
Zhi Zeng <zhzeng@linkedin.com>
2022-10-04 14:05:54 -0700

    Use the getApplication IDs API from YarnUtils, and use scan ID from log as fallback logic

--- az-hadoop-jobtype-plugin/src/main/java/azkaban/jobtype/HadoopJobUtils.java
+++ az-hadoop-jobtype-plugin/src/main/java/azkaban/jobtype/HadoopJobUtils.java
@@ -15,6 +15,8 @@
  */
 package azkaban.jobtype;
 
+import static azkaban.Constants.FlowProperties.AZKABAN_FLOW_EXEC_ID;
+
 import azkaban.flow.CommonJobProperties;
 import azkaban.security.commons.HadoopSecurityManager;
 import azkaban.security.commons.HadoopSecurityManagerException;
@@ -375,9 +377,40 @@ public class HadoopJobUtils {
   private static void findAndKillYarnApps(Props jobProps, Logger log) {
     final String logFilePath = jobProps.getString(CommonJobProperties.JOB_LOG_FILE);
     log.info("Log file path is: " + logFilePath);
-    Set<String> allSpawnedJobAppIDs = findApplicationIdFromLog(logFilePath, log);
     YarnClient yarnClient = YarnUtils.createYarnClient(jobProps, log);
-    YarnUtils.killAllAppsOnCluster(yarnClient, allSpawnedJobAppIDs, log);
+    Set<String> jobAppIDsToKill = getApplicationIDsToKill(yarnClient, jobProps, log);
+    YarnUtils.killAllAppsOnCluster(yarnClient, jobAppIDsToKill, log);
+  }
+
+
+  /**
+   * Get the yarn applications' ids that needs to be killed (the ones alive / spawned).
+   * First use yarn client to call the cluster, if it fails,
+   * fallback to scan the job log file to look for application ids
+   *
+   * @param yarnClient the started client
+   * @param jobProps   should contain flow execution id, and the job log file's path
+   * @param log        logger
+   * @return the set of application ids
+   */
+  public static Set<String> getApplicationIDsToKill(YarnClient yarnClient, Props jobProps,
+      final Logger log) {
+    Set<String> jobsToKill;
+    try {
+      jobsToKill = YarnUtils.getAllAliveAppIDsByExecID(yarnClient,
+          jobProps.getString(AZKABAN_FLOW_EXEC_ID), log);
+      log.info(String.format("Get alive yarn application IDs from yarn cluster: %s", jobsToKill));
+    } catch (Exception e) {
+      log.warn("fail to get application-ids from yarn, fallback to scan logfile", e);
+
+      final String logFilePath = jobProps.getString(CommonJobProperties.JOB_LOG_FILE);
+      log.info("The job log file path is: " + logFilePath);
+
+      jobsToKill = findApplicationIdFromLog(logFilePath, log);
+      log.info(String.format("Get all spawned yarn application IDs from job log file: %s",
+          jobsToKill));
+    }
+    return jobsToKill;
   }
 
   /**
