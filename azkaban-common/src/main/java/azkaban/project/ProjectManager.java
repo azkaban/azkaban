216b8240d4fc5d94a204546bc403938e038df16a
Utkarsh Kattishettar <ukattishettar@linkedin.com>
2023-03-06 16:43:46 -0800

    Optimizing how proxy users are determinded prior to pod dispatch for … (#3236)
    * Optimizing how proxy users are determinded prior to pod dispatch for Security changes related to POLP commit https://github.com/azkaban/azkaban/pull/3216 and modified tests.
    
    * Making return types as Set interface instead of HashSet and adding comments for getJobTypeUsersForFlow
    
    * Added unit test for getJobTypeUsersForFlow and null check for same
    
    * Adding test for getJobTypeUsersForFlow and reverting unnecessary imports to KubernetesContainerizedImplTest
    
    * Adding changes to address merge conflicts
    
    * Removed extra file added erroneously
    
    * Added comments to explain rational for change. Refactored proxy user prefetch logic to have a fall back to fetch all users and use a threshold to switch back to getting all of them from the project page.
    
    * Adding multi line comments instead of single line comments
    
    * Using empty strings instead of null to avoid repeated checks and added threshold to optimize proxy user prefetching logic
    
    * corrected logic for check prefetchAllProxyUserCredentials and removed extra null check
    
    * Separated parseJobTypeUsersForFlow to parse string from config only once and added to map, modified tests to accomodate this
    
    * Changed varible names and added empty string check in getJobTypeUsersForFlow
    
    * Removing extra comments
    
    * Added null checks to ensure node props which dont exist are caught gracefully and added time indication for logging
    
    * Modified how .flow extension is added when fetching flow properties when jobName is null
    
    * Limited scope of adding flow file extension for null jobs to ContainerImplUtils since the UI gets affected by adding it in ProjectManager
    
    * Adding comment for TODO for flow1.0 in future release
    
    * refactored getProxyUsersForFlow to account for some flow1.0 flows
    
    * Refactored getProxyUsersForFlow to account for flow1.0 flows and moved to optionals
    
    * Removed extra  imports
    
    * Added TODO comments

--- azkaban-common/src/main/java/azkaban/project/ProjectManager.java
+++ azkaban-common/src/main/java/azkaban/project/ProjectManager.java
@@ -312,7 +312,7 @@ public class ProjectManager {
       final String source) throws ProjectManagerException {
     if (FlowLoaderUtils.isAzkabanFlowVersion20(flow.getAzkabanFlowVersion())) {
       // Return the properties from the original uploaded flow file.
-      return getPropertiesFromFlowFile(flow, jobName, source, 1);
+        return getPropertiesFromFlowFile(flow, jobName, source, 1);
     } else {
       return this.projectLoader.fetchProjectProperty(project, source);
     }
